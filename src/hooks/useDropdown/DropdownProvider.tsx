f68d0361da1d23c61bafb42dde7cf4a0e8488bef
jackbuehner <jack.buehner@cristata.app>
2021-04-12 02:23:22 -0400

    remove unused import

--- src/hooks/useDropdown/DropdownProvider.tsx
+++ src/hooks/useDropdown/DropdownProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import { DropdownContext } from './_DropdownContext';
 import { DropdownPortal } from './_DropdownPortal';
 
