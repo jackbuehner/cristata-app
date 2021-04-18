2c77e0aaef9da93e15f9a268f5db7f8ef66d1a67
jackbuehner <jack.buehner@cristata.app>
2021-04-18 19:26:08 -0400

    fix icon button: don't add 'px' to width prop string

--- src/components/Button/IconButton.tsx
+++ src/components/Button/IconButton.tsx
@@ -17,7 +17,7 @@ const BUTTON = styled.button<StyledButtonProps>`
   align-items: center;
   justify-content: center;
   padding: 4px;
-  width: ${({ width }) => (width ? width + 'px' : 'unset')};
+  width: ${({ width }) => (width ? width : 'unset')};
   height: ${({ height }) => (height ? height : '30px')};
   border-radius: ${({ borderRadius, theme }) =>
     borderRadius?.base !== undefined ? borderRadius.base : theme.radius};
