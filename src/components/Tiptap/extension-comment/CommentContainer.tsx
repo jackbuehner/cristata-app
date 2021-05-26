00bc17f498c1f36101b2307c99e5cb75ff9f3cec
jackbuehner <jack.buehner@cristata.app>
2021-05-26 16:29:11 -0400

    add initial version of comment tiptap extension

---
+++ src/components/Tiptap/extension-comment/CommentContainer.tsx
@@ -0,0 +1,179 @@
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Comment20Regular } from '@fluentui/react-icons';
+import { NodeViewWrapper, NodeViewContent, NodeViewProps, Node } from '@tiptap/react';
+import React, { useEffect, useRef, useState } from 'react';
+import { themeType } from '../../../utils/theme/theme';
+import { IconButton } from '../../Button';
+import { TextArea } from '../../TextArea';
+import { CommentOptions } from './comment';
+
+interface ICommentContainer extends NodeViewProps {
+  extension: Node<CommentOptions>;
+}
+
+function CommentContainer(props: ICommentContainer) {
+  const theme = useTheme() as themeType;
+  const messageRef = useRef<HTMLTextAreaElement>(null);
+
+  /**
+   * When the user types in the textarea, update the message attribute
+   * with the new message AND make sure that the textarea height matches the message
+   */
+  const handleCommentMessageChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    props.updateAttributes({
+      ...props.node.attrs,
+      message: event.currentTarget.value,
+    });
+    if (messageRef.current) {
+      messageRef.current.style.height = `auto`;
+      messageRef.current.style.height = `${messageRef.current.scrollHeight}px`;
+    }
+  };
+
+  // store the position and size information of the last element that executed `toggleCard()`
+  const [triggerRect, setTriggerRect] = useState<DOMRect>(new DOMRect());
+
+  // control whether the card is shown
+  const [isShown, setIsShown] = useState<boolean>(false);
+
+  /**
+   * Toggle the card when a button is clicked
+   */
+  const toggleCard = (e: React.MouseEvent) => {
+    setIsShown(!isShown);
+    setTriggerRect(e.currentTarget.getBoundingClientRect());
+    console.log(e.currentTarget.getBoundingClientRect());
+  };
+
+  /**
+   * Listen for any scroll events on the page
+   * and hide the comment card if they occur.
+   */
+  useEffect(() => {
+    const closeOnScroll = () => {
+      setIsShown(false);
+    };
+    document.addEventListener('scroll', closeOnScroll, true);
+    return document.removeEventListener('scroll', closeOnScroll);
+  }, [setIsShown]);
+
+  return (
+    <NodeViewWrapper as={`span`}>
+      <NodeViewContent as={`span`} style={{ backgroundColor: props.node.attrs.color }}></NodeViewContent>
+      <ToggleCardButton icon={<Comment20Regular />} onClick={toggleCard} />
+      {isShown ? (
+        <Card theme={theme} contentEditable={false} triggerRect={triggerRect}>
+          <Meta contentEditable={false}>
+            <ProfilePhoto theme={theme} src={props.node.attrs.commenter.photo} contentEditable={false} />
+            <div contentEditable={false}>
+              <Commenter theme={theme} contentEditable={false}>
+                {props.node.attrs.commenter.name}
+              </Commenter>
+              <Timestamp theme={theme} contentEditable={false}>
+                {props.node.attrs.timestamp}
+              </Timestamp>
+            </div>
+          </Meta>
+          <Message
+            theme={theme}
+            onChange={handleCommentMessageChange}
+            rows={1}
+            value={props.node.attrs.message}
+            ref={messageRef}
+            placeholder={`Type a comment`}
+          />
+        </Card>
+      ) : null}
+    </NodeViewWrapper>
+  );
+}
+
+const ToggleCardButton = styled(IconButton)`
+  height: 18px;
+  width: 18px;
+  position: absolute;
+  display: inline-flex;
+  margin-top: -8px;
+  margin-left: -6px;
+  padding: 2px;
+  background-color: transparent;
+  border-color: transparent;
+  &:hover,
+  &:active {
+    background-color: #e0e0e0;
+    border-color: rgba(148, 148, 148, 0.2);
+  }
+  &:hover:active {
+    background-color: #dddddd;
+    border-color: rgba(148, 148, 148, 0.25);
+  }
+`;
+
+const Card = styled.div<{ theme: themeType; triggerRect: DOMRect }>`
+  position: fixed;
+  background: white;
+  box-sizing: border-box;
+  padding: 16px 20px;
+  margin: 0;
+  width: 280px;
+  box-shadow: rgb(0 0 0 / 13%) 0px 1.6px 3.6px 0px, rgb(0 0 0 / 11%) 0px 0.3px 0.9px 0px;
+  border: 1px solid lightgray;
+  top: ${({ triggerRect }) => triggerRect.y + triggerRect.height}px;
+  left: ${({ triggerRect }) =>
+    triggerRect.x + triggerRect.width / 2 + 280 > document.body.offsetWidth
+      ? document.body.offsetWidth - 300
+      : triggerRect.x + triggerRect.width / 2 - 140}px;
+  border-radius: ${({ theme }) => theme.radius};
+  *::selection {
+    background-color: transparent !important;
+  }
+`;
+
+const Meta = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: left;
+  gap: 10px;
+  > div {
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
+  }
+`;
+
+const ProfilePhoto = styled.img<{ theme: themeType }>`
+  width: 28px;
+  height: 28px;
+  border-radius: ${({ theme }) => theme.radius};
+`;
+
+const Commenter = styled.span<{ theme: themeType }>`
+  font-size: 14px;
+  line-height: 14px;
+  font-family: ${({ theme }) => theme.font.detail};
+  font-weight: 600;
+  color: ${({ theme }) => theme.color.neutral[theme.mode][1200]};
+`;
+
+const Timestamp = styled.span<{ theme: themeType }>`
+  font-size: 11px;
+  line-height: 11px;
+  font-family: ${({ theme }) => theme.font.detail};
+  color: ${({ theme }) => theme.color.neutral[theme.mode][800]};
+`;
+
+const Message = styled(TextArea)<{ theme: themeType }>`
+  resize: none;
+  overflow-y: hidden;
+  box-shadow: none !important;
+  width: calc(100% + 40px);
+  margin: 0 0 -24px -20px;
+  padding: 10px 20px 18px;
+  &::selection {
+    background-color: #c4dffc !important;
+  }
+`;
+
+export { CommentContainer };
---
+++ src/components/Tiptap/extension-comment/comment.tsx
@@ -0,0 +1,264 @@
+import { Node } from '@tiptap/core';
+import { ReactNodeViewRenderer } from '@tiptap/react';
+import { Command } from '@tiptap/react';
+import { Node as ProsemirrorNode, Slice } from 'prosemirror-model';
+import { TextSelection } from 'prosemirror-state';
+import { CommentContainer } from './CommentContainer';
+import { v4 as uuidv4 } from 'uuid';
+
+declare module '@tiptap/core' {
+  interface Commands {
+    comment: {
+      /**
+       * Wrap text nodes in a comment node.
+       */
+      setComment: () => Command;
+      /**
+       * Remove the comment node included in the selection.
+       *
+       * Any child text nodes will be reinserted.
+       *
+       * This command will expand the selection to include the entiere comment node.
+       */
+      unsetComment: () => Command;
+    };
+  }
+}
+
+interface CommentOptions {}
+
+const Comment = Node.create<CommentOptions>({
+  name: 'comment',
+
+  // add to 'inline' group
+  group: 'inline',
+
+  // renders comment nodes in line with the text
+  inline: true,
+
+  // only allow zero or more inline nodes
+  content: 'text*',
+
+  /**
+   *
+   */
+  addAttributes() {
+    return {
+      // use a color attrubute to define the background color of text highlighted with a comment
+      color: {
+        // use this color by default
+        default: '#faf0a2',
+        // apply these attributes to the rendered element in the editor
+        renderHTML: (attributes) => {
+          return {
+            style: `background-color: ${attributes.color}`,
+          };
+        },
+        parseHTML: (element) => ({
+          color: element.style.backgroundColor || '#faf0a2',
+        }),
+      },
+      message: {
+        default: '',
+        renderHTML: (attributes) => ({
+          'data-message': attributes.message,
+        }),
+        parseHTML: (element) => ({
+          message: element.getAttribute('data-message') || '',
+        }),
+      },
+      timestamp: {
+        default: new Date().toISOString,
+        renderHTML: (attributes) => ({
+          'data-timestamp': attributes.timestamp,
+        }),
+        parseHTML: (element) => ({
+          timestamp: element.getAttribute('data-timestamp') || new Date(0).toISOString(),
+        }),
+      },
+      commenter: {
+        default: {
+          name: '',
+          photo: 'https://avatars.githubusercontent.com/u/69555023',
+        },
+        renderHTML: (attributes) => ({
+          'data-commenter': JSON.stringify(attributes.commenter),
+        }),
+        parseHTML: (element) => {
+          const attr = element.getAttribute('data-commenter');
+          const commenter = JSON.parse(
+            attr || '{ "name": "hi", "photo": "https://avatars.githubusercontent.com/u/69555023" }'
+          );
+          return {
+            commenter: commenter,
+          };
+        },
+      },
+      uuid: {
+        default: uuidv4(),
+        renderHTML: (attributes) => ({
+          'data-uuid': attributes.uuid,
+        }),
+        parseHTML: (element) => ({
+          uuid: element.getAttribute('data-uuid') || uuidv4(),
+        }),
+      },
+    };
+  },
+
+  /**
+   *
+   */
+  renderHTML({ HTMLAttributes }) {
+    return ['commment', HTMLAttributes, 0];
+  },
+
+  /**
+   *
+   */
+  parseHTML() {
+    return [{ tag: 'comment' }];
+  },
+
+  /**
+   *
+   */
+  addCommands() {
+    return {
+      setComment:
+        () =>
+        ({ state, tr, dispatch }) => {
+          // a slice containing the selected nodes
+          const selectionSlice = state.selection.content();
+
+          // keep track of whether the selection includes the comment so that we do no insert
+          // overlapping comments
+          let selectionIncludesComment = false;
+          selectionSlice.content.descendants((node) => {
+            if (node.type === this.type) selectionIncludesComment = true;
+          });
+
+          if (dispatch) {
+            // dispatch is undefined when testing whether the command is possible with `can()`
+
+            if (selectionSlice && selectionSlice.content.childCount === 1 && !selectionIncludesComment) {
+              // obtain all text nodes located in the first child node of the slice
+              // NOTES:
+              // - only look in the first child node since comments cannot span
+              //   more than one block node)
+              // - only nodes that are children to the first child node will be
+              //   text nodes)
+              // - continuing when the slice contains more than one node would
+              //   cause two block nodes to collapse into one since the
+              //   `replaceSelectionWith` will remove and ends and starts to
+              //   block nodes
+              // - continuing when the slice contains an existing comment would
+              //   result in overlapping comments
+              textNodesFromSliceFirstChild(selectionSlice).then((textNodes) => {
+                if (textNodes) {
+                  // create a new comment node that contains all of the text nodes
+                  const newCommentNode = this.type.createAndFill({}, textNodes);
+                  if (newCommentNode) {
+                    // replace the current selection with the comment node that
+                    // contains the text nodes in the selection
+                    tr.replaceSelectionWith(newCommentNode, false);
+                    this.editor.view.dispatch(tr);
+                  }
+                }
+              });
+            }
+          }
+
+          // when the selection slice only contains one child,
+          // tell tiptap that this command is possible
+          return selectionSlice.content.childCount === 1 && !selectionIncludesComment;
+        },
+      unsetComment:
+        () =>
+        ({ chain, commands, state, tr, dispatch }) => {
+          return chain()
+            .command(({ tr }) => {
+              // get the parent node, which is the entire comment node if the anchor is inside the comment
+              const parent = tr.selection.$anchor;
+              // check whether the node is a comment node
+              const isComment = parent.node().hasMarkup(this.type);
+              return isComment;
+            })
+            .command(({ tr, chain }) => {
+              // get the entire comment node
+              const parent = tr.selection.$anchor;
+
+              // also store the start position of the parent
+              const start = parent.start();
+
+              // get the text nodes in the comment node
+              const textNodes: ProsemirrorNode<any>[] = [];
+              parent.node().content.descendants((textNode) => {
+                textNodes.push(textNode);
+              });
+
+              // select the entire comment node and delete it
+              tr.setSelection(TextSelection.near(parent));
+              chain().selectParentNode().deleteSelection().run();
+
+              // get the type of the node that is the parent of the comment node
+              // (usually a paragraph)
+              const parentOfParentType = tr.doc.resolve(start - 1).node().type;
+
+              // create a new node that matches the new parent type
+              const newNode = parentOfParentType.createAndFill({}, textNodes);
+
+              if (newNode) {
+                // replace the empty space left after removing the comment and
+                // replace it with the slice, which has the text nodes inside
+                // of the comment node
+                const slice = new Slice(newNode.content, 0, 0);
+                tr.replace(start - 1, start - 1, slice);
+              }
+
+              return true;
+            })
+            .run();
+        },
+    };
+  },
+
+  addNodeView() {
+    return ReactNodeViewRenderer(CommentContainer);
+  },
+});
+
+/**
+ * Get text nodes (recursively) from a slice.
+ * @returns array of TextNodes; null if no nodes in slice
+ */
+async function textNodesFromSliceFirstChild(slice: Slice<any>) {
+  // store any text noted that have been found
+  let TextNodes: ProsemirrorNode<any>[] = [];
+
+  /**
+   * Find the text nodes in a slice.
+   */
+  const findTextNodes = async (node: ProsemirrorNode<any>) => {
+    node.forEach(async (childNode) => {
+      // if it is a text node, add it to the array
+      if (childNode.isText) TextNodes.push(childNode);
+
+      if (childNode.childCount !== 0) {
+        childNode.forEach(async (child) => {
+          await findTextNodes(child);
+        });
+      }
+    });
+  };
+
+  // start the function that recursively looks for text nodes
+  if (slice.content.firstChild) {
+    await findTextNodes(slice.content.firstChild);
+    return TextNodes;
+  }
+  return null;
+}
+
+export { Comment };
+export type { CommentOptions };
