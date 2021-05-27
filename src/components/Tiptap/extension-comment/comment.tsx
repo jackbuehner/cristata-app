aa375725809bdbddce7d8dc490a3f4d19cd6fd14
jackbuehner <jack.buehner@cristata.app>
2021-05-27 04:59:28 -0400

    wrap custom commands in try-catch

--- src/components/Tiptap/extension-comment/comment.tsx
+++ src/components/Tiptap/extension-comment/comment.tsx
@@ -128,95 +128,109 @@ const Comment = Node.create<CommentOptions>({
       setComment:
         () =>
         ({ state, tr, dispatch }) => {
-          // a slice containing the selected nodes
-          const selectionSlice = state.selection.content();
+          try {
+            // a slice containing the selected nodes
+            const selectionSlice = state.selection.content();
 
-          // keep track of whether the selection includes the comment so that we do no insert
-          // overlapping comments
-          let selectionIncludesComment = false;
-          selectionSlice.content.descendants((node) => {
-            if (node.type === this.type) selectionIncludesComment = true;
-          });
+            // keep track of whether the selection includes the comment so that we do no insert
+            // overlapping comments
+            let selectionIncludesComment = false;
+            selectionSlice.content.descendants((node) => {
+              if (node.type === this.type) selectionIncludesComment = true;
+            });
 
-          if (dispatch) {
-            // dispatch is undefined when testing whether the command is possible with `can()`
+            if (dispatch) {
+              // dispatch is undefined when testing whether the command is possible with `can()`
 
-            if (selectionSlice && selectionSlice.content.childCount === 1 && !selectionIncludesComment) {
-              // obtain all text nodes located in the first child node of the slice
-              // NOTES:
-              // - only look in the first child node since comments cannot span
-              //   more than one block node)
-              // - only nodes that are children to the first child node will be
-              //   text nodes)
-              // - continuing when the slice contains more than one node would
-              //   cause two block nodes to collapse into one since the
-              //   `replaceSelectionWith` will remove and ends and starts to
-              //   block nodes
-              // - continuing when the slice contains an existing comment would
-              //   result in overlapping comments
-              textNodesFromSliceFirstChild(selectionSlice).then((textNodes) => {
-                if (textNodes) {
-                  // create a new comment node that contains all of the text nodes
-                  const newCommentNode = this.type.createAndFill({}, textNodes);
-                  if (newCommentNode) {
-                    // replace the current selection with the comment node that
-                    // contains the text nodes in the selection
-                    tr.replaceSelectionWith(newCommentNode, false);
-                    this.editor.view.dispatch(tr);
+              if (selectionSlice && selectionSlice.content.childCount === 1 && !selectionIncludesComment) {
+                // obtain all text nodes located in the first child node of the slice
+                // NOTES:
+                // - only look in the first child node since comments cannot span
+                //   more than one block node)
+                // - only nodes that are children to the first child node will be
+                //   text nodes)
+                // - continuing when the slice contains more than one node would
+                //   cause two block nodes to collapse into one since the
+                //   `replaceSelectionWith` will remove and ends and starts to
+                //   block nodes
+                // - continuing when the slice contains an existing comment would
+                //   result in overlapping comments
+                textNodesFromSliceFirstChild(selectionSlice).then((textNodes) => {
+                  if (textNodes) {
+                    // create a new comment node that contains all of the text nodes
+                    const newCommentNode = this.type.createAndFill({}, textNodes);
+                    if (newCommentNode) {
+                      // replace the current selection with the comment node that
+                      // contains the text nodes in the selection
+                      tr.replaceSelectionWith(newCommentNode, false);
+                      this.editor.view.dispatch(tr);
+                    }
                   }
-                }
-              });
+                });
+              }
             }
-          }
 
-          // when the selection slice only contains one child,
-          // tell tiptap that this command is possible
-          return selectionSlice.content.childCount === 1 && !selectionIncludesComment;
+            // when the selection slice only contains one child,
+            // tell tiptap that this command is possible
+            return selectionSlice.content.childCount === 1 && !selectionIncludesComment;
+          } catch (error) {
+            console.error(error);
+            return false;
+          }
         },
       unsetComment:
         () =>
         ({ chain, commands, state, tr, dispatch }) => {
           return chain()
             .command(({ tr }) => {
-              // get the parent node, which is the entire comment node if the anchor is inside the comment
-              const parent = tr.selection.$anchor;
-              // check whether the node is a comment node
-              const isComment = parent.node().hasMarkup(this.type);
-              return isComment;
+              try {
+                // get the parent node, which is the entire comment node if the anchor is inside the comment
+                const parent = tr.selection.$anchor;
+                // check whether the node is a comment node
+                const isComment = parent.node().hasMarkup(this.type);
+                return isComment;
+              } catch (error) {
+                console.error(error);
+                return false;
+              }
             })
             .command(({ tr, chain }) => {
-              // get the entire comment node
-              const parent = tr.selection.$anchor;
+              try {
+                // get the entire comment node
+                const parent = tr.selection.$anchor;
 
-              // also store the start position of the parent
-              const start = parent.start();
+                // also store the start position of the parent
+                const start = parent.start();
 
-              // get the text nodes in the comment node
-              const textNodes: ProsemirrorNode<any>[] = [];
-              parent.node().content.descendants((textNode) => {
-                textNodes.push(textNode);
-              });
+                // get the text nodes in the comment node
+                const textNodes: ProsemirrorNode<any>[] = [];
+                parent.node().content.descendants((textNode) => {
+                  textNodes.push(textNode);
+                });
 
-              // select the entire comment node and delete it
-              tr.setSelection(TextSelection.near(parent));
-              chain().selectParentNode().deleteSelection().run();
+                // select the entire comment node and delete it
+                tr.setSelection(TextSelection.near(parent));
+                chain().selectParentNode().deleteSelection().run();
 
-              // get the type of the node that is the parent of the comment node
-              // (usually a paragraph)
-              const parentOfParentType = tr.doc.resolve(start - 1).node().type;
+                // get the type of the node that is the parent of the comment node
+                // (usually a paragraph)
+                const parentOfParentType = tr.doc.resolve(start).node().type;
 
-              // create a new node that matches the new parent type
-              const newNode = parentOfParentType.createAndFill({}, textNodes);
+                // create a new node that matches the new parent type
+                const newNode = parentOfParentType.createAndFill({}, textNodes);
 
-              if (newNode) {
-                // replace the empty space left after removing the comment and
-                // replace it with the slice, which has the text nodes inside
-                // of the comment node
-                const slice = new Slice(newNode.content, 0, 0);
-                tr.replace(start - 1, start - 1, slice);
+                if (newNode) {
+                  // replace the empty space left after removing the comment and
+                  // replace it with the slice, which has the text nodes inside
+                  // of the comment node
+                  const slice = new Slice(newNode.content, 0, 0);
+                  tr.replace(start - 1, start - 1, slice);
+                }
+                return true;
+              } catch (error) {
+                console.error(error);
+                return false;
               }
-
-              return true;
             })
             .run();
         },
