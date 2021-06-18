f05e429a0fa46dc462a72481c3cd5ddef99e0b5a
jackbuehner <jack.buehner@cristata.app>
2021-06-18 17:19:58 -0600

    enable adding a spinner to sidenav headings when they are loading

--- src/components/Heading/SideNavHeading.tsx
+++ src/components/Heading/SideNavHeading.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
+import { CircularProgress } from '@material-ui/core';
 import { themeType } from '../../utils/theme/theme';
 
 const SideNavHeadingComponent = styled.div<{ theme: themeType }>`
@@ -7,12 +8,26 @@ const SideNavHeadingComponent = styled.div<{ theme: themeType }>`
   font-weight: 600;
   font-size: 16px;
   margin: 20px 10px 10px 10px;
+  position: relative;
 `;
 
-function SideNavHeading(props: { children: React.ReactNode }) {
+const Spinner = styled(CircularProgress)<{ theme: themeType }>`
+  width: 20px !important;
+  height: 20px !important;
+  right: 0;
+  position: absolute;
+  font-family: ${({ theme }) => theme.color.primary[900]} !important;
+`;
+
+function SideNavHeading(props: { children: React.ReactNode; isLoading?: boolean }) {
   const theme = useTheme() as themeType;
 
-  return <SideNavHeadingComponent theme={theme}>{props.children}</SideNavHeadingComponent>;
+  return (
+    <SideNavHeadingComponent theme={theme}>
+      {props.children}
+      {props.isLoading ? <Spinner theme={theme} /> : null}
+    </SideNavHeadingComponent>
+  );
 }
 
 export { SideNavHeading };
