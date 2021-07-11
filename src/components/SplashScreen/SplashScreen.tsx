34c0f6d4a65c6b18ce114400fa87a5ddf5959f4b
Jack Buehner <github@jackbuehner.com>
2021-07-10 19:21:49 -0600

    only show app for users who are members
    also added comments to the code

--- src/components/SplashScreen/SplashScreen.tsx
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -5,37 +5,47 @@ import { useHistory } from 'react-router-dom';
 import { themeType } from '../../utils/theme/theme';
 
 interface ISplashScreen {
-  loading: boolean;
-  error: AxiosError<any> | undefined;
-  user: any;
+  loading: boolean; // loading status of api request for user
+  error?: AxiosError<any>; // error for api request
+  user: any; // the user from the api request
 }
 
+/**
+ * Displays a splash screen over the content until the app knows the user is authenticated.
+ *
+ * Also contains the logic for redirecting the user to or from the sign in page route.
+ */
 function SplashScreen(props: ISplashScreen) {
   const theme = useTheme() as themeType;
   const history = useHistory();
 
+  // redirect the user to sign in page if not authenticated
   useEffect(() => {
+    // store where the user should be redirected
     localStorage.setItem(
       'auth.redirect_after',
       `${window.location.pathname}${window.location.hash}${window.location.search}`
     );
     if (props.error) {
       const is403 = props.error.message.indexOf('403') !== -1;
+      // if the error is a 403 (not authenticated), redirect to sign in page
       if (is403) {
         history.push(`/sign-in`);
       }
     }
   }, [props.error, history]);
 
+  // set the user to localstorage if the user is authenticated and is a member
   useEffect(() => {
-    if (props.user) {
-      localStorage.setItem('auth.user', JSON.stringify(props.user));
+    if (props.user && props.user.member_status) {
+      localStorage.setItem('auth.user', JSON.stringify(props.user)); // set user
       const redirect = localStorage.getItem('auth.redirect_after') || '/';
-      history.push(redirect !== '/sign-in' ? redirect : '/');
-      localStorage.removeItem('auth.redirect_after');
+      history.push(redirect !== '/sign-in' ? redirect : '/'); // redirect
+      localStorage.removeItem('auth.redirect_after'); // remove redirect url from localstorage
     }
   }, [props.user, history]);
 
+  // set the session id
   useEffect(() => {
     const sessionIdIsSet = !!sessionStorage.getItem('sessionId');
     if (!sessionIdIsSet) {
