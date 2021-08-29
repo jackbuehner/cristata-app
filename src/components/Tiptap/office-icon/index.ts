4d6fb41e5cc480e8f6426cd54248180b768f14da
Jack Buehner <github@jackbuehner.com>
2021-08-29 17:15:34 -0400

    add review tab icons

---
+++ src/components/Tiptap/office-icon/index.ts
@@ -0,0 +1,9 @@
+import { TrackChanges20Icon } from './TrackChanges20Icon';
+import { AcceptRevision20Icon } from './AcceptRevision20Icon';
+import { RejectRevision20Icon } from './RejectRevision20Icon';
+import { NextRevision20Icon } from './NextRevision20Icon';
+import { PreviousRevision20Icon } from './PreviousRevision20Icon';
+import { Editor20Icon } from './Editor20Icon';
+import { WordCountList20Icon } from './WordCountList20Icon';
+
+export { TrackChanges20Icon, AcceptRevision20Icon, RejectRevision20Icon, NextRevision20Icon, PreviousRevision20Icon, Editor20Icon, WordCountList20Icon };
