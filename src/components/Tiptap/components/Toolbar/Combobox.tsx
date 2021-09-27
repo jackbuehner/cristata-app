246c23b976d98d9fa38abfaa29b98ae6993495c0
Jack Buehner <github@jackbuehner.com>
2021-09-27 03:21:05 +0000

    show dropdown arrow on comboxes

--- src/components/Tiptap/components/Toolbar/Combobox.tsx
+++ src/components/Tiptap/components/Toolbar/Combobox.tsx
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css, SerializedStyles, useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
-import { themeType } from '../../../../utils/theme/theme';
+import { ChevronDown12Regular } from '@fluentui/react-icons';
+import { colorType, themeType } from '../../../../utils/theme/theme';
 import { Button } from '../../../Button';
 import { ButtonProps } from '../../../Button/Button';
 import { ToolbarRowButton } from './ToolbarRowButton';
@@ -9,24 +10,39 @@ import { ToolbarRowButton } from './ToolbarRowButton';
 interface ICombobox extends ButtonProps {
   width: string;
   cssContainerExtra?: SerializedStyles;
+  color: colorType;
 }
 
 function Combobox(props: ICombobox) {
   const theme = useTheme() as themeType;
-  const Box = styled(Button)<{ theme: themeType; width: string }>`
+  const Box = styled(Button)<{
+    theme: themeType;
+    width: string;
+    side: 'left' | 'right';
+  }>`
     background-color: ${({ theme }) =>
       theme.mode === 'light' ? 'white' : 'black'};
     height: 28px;
     min-width: 40px;
     box-shadow: none !important;
-    border: 1px solid ${({ theme }) =>
+    border-color: ${({ theme }) =>
       theme.color.neutral[theme.mode][800]} !important;
+    border-width: 1px;
+    border-left-width: ${({ side }) => (side === 'right' ? 0 : 1)}px !important;
+    border-radius: 0 !important;
+    border-right-width: ${({ side }) => (side === 'left' ? 0 : 1)}px !important;
     width: ${({ width }) => width};
     justify-content: left;
+    ${({ side }) =>
+      side === 'right'
+        ? `
+            padding: 2px;
+            min-width: 16px;
+          `
+        : null}
   `;
   return (
     <ToolbarRowButton
-      {...props}
       onClick={undefined}
       isActive={false}
       theme={theme}
@@ -37,16 +53,37 @@ function Combobox(props: ICombobox) {
         ${props.cssContainerExtra}
       `}
     >
-      <Box
-        {...props}
-        theme={theme}
-        cssExtra={css`
+      <WrapperButton onClick={props.onClick} disabled={props.disabled}>
+        <Box
+          width={props.width}
+          children={props.children}
+          theme={theme}
+          color={props.color}
+          side={'left'}
+          disabled={props.disabled}
+          cssExtra={css`
           overflow: hidden;
           ${props.cssExtra}
         `}
-      ></Box>
+        />
+        <Box
+          width={'16px'}
+          theme={theme}
+          color={props.color}
+          side={'right'}
+          disabled={props.disabled}
+        >
+          <ChevronDown12Regular />
+        </Box>
+      </WrapperButton>
     </ToolbarRowButton>
   );
 }
+
+const WrapperButton = styled.button`
+  all: unset;
+  display: flex;
+  flex-direction: row;
+`;
 
 export { Combobox };
