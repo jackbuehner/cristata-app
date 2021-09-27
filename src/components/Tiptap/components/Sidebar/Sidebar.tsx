84585ea9d254967aa24bf7671ceb902b764c3520
Jack Buehner <github@jackbuehner.com>
2021-09-27 02:04:12 +0000

    add sidebar component

---
+++ src/components/Tiptap/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,56 @@
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+import { themeType } from '../../../../utils/theme/theme';
+import { SidebarHeader } from './SidebarHeader';
+
+interface I_SIDEBAR {
+  theme: themeType;
+  isOpen: boolean;
+}
+
+const SIDEBAR = styled.div<I_SIDEBAR>`
+  background-color: ${({ theme }) => theme.color.neutral[theme.mode][100]};
+  width: ${({ isOpen }) => (isOpen ? 330 : 0)}px;
+  height: 100%;
+  flex-shrink: 0;
+  border-left: 1px solid ${({ theme, isOpen }) =>
+    isOpen ? theme.color.neutral[theme.mode][500] : 'transparent'};
+  transition: width 160ms cubic-bezier(0.165, 0.84, 0.44, 1) 0s, opacity 40ms ease;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  positon: relative;
+  opacity: ${({ isOpen }) => (isOpen ? 1 : 0)};
+`;
+
+const Container = styled.div`
+  overflow-y: auto;
+  height: 100%;
+  width: 100%;
+  > div {
+    padding: 12px;
+  }
+`;
+
+interface ISidebar {
+  closeFunction?: () => void;
+  isOpen: boolean;
+  children?: React.ReactNode;
+  header: string;
+}
+
+function Sidebar(props: ISidebar) {
+  const theme = useTheme() as themeType;
+
+  return (
+    <SIDEBAR theme={theme} isOpen={props.isOpen}>
+      <SidebarHeader closeFunction={props.closeFunction}>
+        {props.header}
+      </SidebarHeader>
+      <Container>
+        <div>{props.children}</div>
+      </Container>
+    </SIDEBAR>
+  );
+}
+
+export { Sidebar };
---
+++ src/components/Tiptap/components/Sidebar/SidebarHeader.tsx
@@ -0,0 +1,61 @@
+import { css, useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Dismiss12Regular } from '@fluentui/react-icons';
+import { themeType } from '../../../../utils/theme/theme';
+import { IconButton } from '../../../Button';
+
+const Container = styled.div<{ theme: themeType }>`
+  padding: 8px 12px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: ${({ theme }) => theme.color.neutral[theme.mode][200]};
+`;
+
+const Title = styled.h1<{ theme: themeType }>`
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 30px;
+  height: 30px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  flex-grow: 1;
+  margin: 0;
+  padding: 0;
+  font-family: ${({ theme }) => theme.font.detail};
+  color: ${({ theme }) => theme.color.neutral[theme.mode][1400]};
+`;
+
+interface ISidebarHeader {
+  closeFunction?: () => void;
+  children: React.ReactText;
+}
+
+function SidebarHeader(props: ISidebarHeader) {
+  const theme = useTheme() as themeType;
+
+  return (
+    <Container theme={theme}>
+      <Title theme={theme}>{props.children}</Title>
+      {props.closeFunction ? (
+        <IconButton
+          onClick={props.closeFunction}
+          icon={<Dismiss12Regular />}
+          color={'neutral'}
+          backgroundColor={{ base: 'transparent' }}
+          border={{ base: '1px solid transparent' }}
+          cssExtra={css`
+          span > svg {
+            width: 16px;
+            height: 16px;
+          }
+        `}
+          width={`30px`}
+          height={`30px`}
+        />
+      ) : null}
+    </Container>
+  );
+}
+
+export { SidebarHeader };
