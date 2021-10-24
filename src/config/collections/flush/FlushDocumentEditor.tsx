77541666f9c338c0f3c678ca0472173a7f0ce3f3
Jack Buehner <github@jackbuehner.com>
2021-10-24 23:00:55 +0000

    feat(royal-flush): add export to pdf

--- src/config/collections/flush/FlushDocumentEditor.tsx
+++ src/config/collections/flush/FlushDocumentEditor.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled/macro';
+import { Button } from '../../../components/Button';
 import { CustomFieldProps } from '../../../pages/CMS/ItemDetailsPage/ItemDetailsPage';
 import { Advertisement } from './Advertisement';
 import { FeaturedArticle } from './FeaturedArticle';
@@ -6,25 +7,77 @@ import { Footer } from './Footer';
 import { Header } from './Header';
 import { MoreArticles } from './MoreArticles';
 import { Upcoming } from './Upcoming';
+import html2pdf from 'html2pdf.js';
+import roman from 'romans';
+import { DocumentPdf20Regular } from '@fluentui/react-icons';
+import { css } from '@emotion/react';
 
 function FlushDocumentEditor(props: CustomFieldProps) {
+  const savePdf = () => {
+    const element = document.getElementById('printarea');
+    const options = {
+      // margins are already built in to the element
+      margin: 0,
+      // set filename to the same as the page title
+      filename: `The Royal Flush Vol. ${
+        !isNaN(parseInt(props.state.fields['volume']))
+          ? roman.romanize(parseInt(props.state.fields['volume']))
+          : '??'
+      } Iss. ${props.state.fields['issue'] || '??'}.pdf`,
+      // default quality image
+      image: { type: 'jpeg', quality: 0.98 },
+      // do not convert links to links in the pdf
+      enabledLinks: false,
+      // use higher scale so that it is not pixelated when zoomed
+      html2canvas: { scale: 6 },
+      jsPDF: { format: [612, 792] }, // set to letter size (specifying 'letter' should worl, but causes an error)
+    };
+    html2pdf().from(element).set(options).save();
+  };
+
   return (
-    <Page id={'printarea'}>
-      <Header {...props} />
-      <Columns>
-        <Column>
-          <Upcoming height={'6.80in'} {...props} />
-          <Advertisement {...props} />
-        </Column>
-        <Column>
-          <FeaturedArticle height={'6.51in'} {...props} />
-          <MoreArticles height={'2.38in'} {...props} />
-          <Footer />
-        </Column>
-      </Columns>
-    </Page>
+    <>
+      <ActionsRow>
+        <Button
+          onClick={savePdf}
+          icon={<DocumentPdf20Regular />}
+          cssExtra={css`
+            > .IconStyleWrapper {
+              height: 20px !important;
+            }
+          `}
+        >
+          Export PDF
+        </Button>
+      </ActionsRow>
+      <div id={'printarea'} style={{ width: '9in', height: 'fit-content' }}>
+        <Page>
+          <Header {...props} />
+          <Columns>
+            <Column>
+              <Upcoming height={'6.80in'} {...props} />
+              <Advertisement {...props} />
+            </Column>
+            <Column>
+              <FeaturedArticle height={'6.51in'} {...props} />
+              <MoreArticles height={'2.38in'} {...props} />
+              <Footer />
+            </Column>
+          </Columns>
+        </Page>
+      </div>
+    </>
   );
 }
+
+const ActionsRow = styled.div`
+  display: flex;
+  width: 8.5in;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  margin-bottom: 20px;
+`;
 
 const Page = styled.div`
   width: 8.5in;
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -6,3 +6,8 @@ declare module 'react-use-dimensions' {
 declare module '@cristata/prosemirror-to-html-js' {
   export { Renderer, Node, Mark };
 }
+
+declare module 'html2pdf.js' {
+  const html2pdf;
+  export = html2pdf;
+}
