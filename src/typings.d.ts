a8f1139f77f7ffa5626022619b540025f7c85884
Jack Buehner <github@jackbuehner.com>
2021-10-24 00:01:05 +0000

    feat: add tiptap to html package

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,3 +2,7 @@ declare module 'react-use-dimensions' {
   var useDimensions: any;
   export = useDimensions;
 }
+
+declare module '@cristata/prosemirror-to-html-js' {
+  export { Renderer, Node, Mark };
+}
