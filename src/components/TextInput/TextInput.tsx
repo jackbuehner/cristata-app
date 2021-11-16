b7263c4a0bfeec3e66bd048995f7b89f8d0575f9
Jack Buehner <github@jackbuehner.com>
2021-11-16 23:35:31 +0000

    feat: support onBlur event for TextInput

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -14,6 +14,7 @@ interface ITextInputComponent extends ITextInputBase {
 interface ITextInput extends ITextInputBase {
   onFocus?: (e: React.FocusEvent<HTMLInputElement>) => void;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
   value?: string;
   name?: string;
   title?: string;
@@ -29,20 +30,17 @@ const TextInputComponent = styled.input<ITextInputComponent>`
   box-sizing: border-box;
   border-radius: ${({ theme }) => theme.radius};
   border: none;
-  box-shadow: ${({ theme }) =>
-    theme.color.neutral[theme.mode][800]} 0px 0px 0px 1px inset;
+  box-shadow: ${({ theme }) => theme.color.neutral[theme.mode][800]} 0px 0px 0px 1px inset;
   transition: box-shadow 240ms;
   font-family: ${({ theme, font }) => theme.font[font ? font : 'detail']};
   font-size: 14px;
   font-variant-numeric: lining-nums;
   &:hover {
-    box-shadow: ${({ theme }) =>
-      theme.color.neutral[theme.mode][1000]} 0px 0px 0px 1px inset;
+    box-shadow: ${({ theme }) => theme.color.neutral[theme.mode][1000]} 0px 0px 0px 1px inset;
   }
   &:focus {
     outline: none;
-    box-shadow: ${({ theme }) =>
-      theme.color.primary[800]} 0px 0px 0px 2px inset;
+    box-shadow: ${({ theme }) => theme.color.primary[800]} 0px 0px 0px 2px inset;
   }
 `;
 
@@ -54,6 +52,7 @@ function TextInput(props: ITextInput) {
       font={props.font}
       onFocus={props.onFocus}
       onChange={props.onChange}
+      onBlur={props.onBlur}
       value={props.value}
       name={props.name}
       title={props.title}
