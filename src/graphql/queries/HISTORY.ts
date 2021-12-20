9f562e84f6c0ffe7584b153f83746b7b8cb17a63
Jack Buehner <github@jackbuehner.com>
2021-12-20 23:18:43 +0000

    feat: switch recent activity component to new graphql api
    The new API provides details for all collections. It also only returns documents with permission for the current user.
    With the new data, we have a new function to list all users involved in the change.

---
+++ src/graphql/queries/HISTORY.ts
@@ -0,0 +1,60 @@
+import { gql } from '@apollo/client';
+import { jsonToGraphQLQuery, VariableType } from 'json-to-graphql-query';
+import { Paged } from '../../interfaces/cristata/paged';
+import { paged } from '../paged';
+
+/**
+ * Get history for cms collections.
+ */
+const HISTORY = gql(
+  jsonToGraphQLQuery({
+    query: {
+      __variables: {
+        limit: 'Int! = 10',
+        page: 'Int = 1',
+        collections: '[String]',
+        exclude: '[String]',
+      },
+      collectionActivity: {
+        __args: {
+          limit: new VariableType('limit'),
+          page: new VariableType('page'),
+          collections: new VariableType('collections'),
+          exclude: new VariableType('exclude'),
+        },
+        ...paged({
+          _id: true,
+          name: true,
+          in: true,
+          user: {
+            _id: true,
+            name: true,
+          },
+          action: true,
+          at: true,
+        }),
+      },
+    },
+  })
+);
+
+type HISTORY__TYPE =
+  | {
+      collectionActivity: Paged<HISTORY__DOC_TYPE>;
+    }
+  | undefined;
+
+type HISTORY__DOC_TYPE = {
+  _id: string;
+  name: string;
+  in: string;
+  user: {
+    _id: string;
+    name: string;
+  };
+  action: string;
+  at: string; // ISO date
+};
+
+export { HISTORY };
+export type { HISTORY__TYPE, HISTORY__DOC_TYPE };
--- src/pages/Home/RecentActivity.tsx
+++ src/pages/Home/RecentActivity.tsx
@@ -1,9 +1,13 @@
+import { useQuery } from '@apollo/client';
 import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled/macro';
-import useAxios from 'axios-hooks';
+import ColorHash from 'color-hash';
 import { DateTime } from 'luxon';
+import { MouseEventHandler } from 'react';
 import { useHistory } from 'react-router-dom';
-import { IProfile } from '../../interfaces/cristata/profiles';
+import { collections } from '../../config';
+import { collection as collectionType } from '../../config/collections';
+import { HISTORY, HISTORY__DOC_TYPE, HISTORY__TYPE } from '../../graphql/queries';
 import { themeType } from '../../utils/theme/theme';
 
 /**
@@ -15,28 +19,89 @@ function RecentActivity() {
   const theme = useTheme() as themeType;
   const history = useHistory();
 
-  interface Ihistory {
-    _id: string;
-    name: string;
-    collection: string;
-    history: Array<{
-      type: string;
-      user: number;
-      at: string;
-    }>;
-  }
+  const { data } = useQuery<HISTORY__TYPE>(HISTORY, {
+    variables: { limit: 25, exclude: ['User'] },
+    fetchPolicy: 'no-cache',
+  });
+  const unmergedHistory = data?.collectionActivity.docs;
+  const activity = (() => {
+    type mergedHistoryType = {
+      _id: HISTORY__DOC_TYPE['_id'];
+      name: HISTORY__DOC_TYPE['name'];
+      in: HISTORY__DOC_TYPE['in'];
+      users: Array<HISTORY__DOC_TYPE['user']>;
+      action: HISTORY__DOC_TYPE['action'];
+      at: HISTORY__DOC_TYPE['at'];
+    };
 
-  const [{ data: itemHistory }] = useAxios<Ihistory[]>(`/history`);
-  const [{ data: profiles }] = useAxios<IProfile[]>(`/users`);
+    let merged: mergedHistoryType[] = [];
+
+    if (unmergedHistory) {
+      for (let i = 0; i < unmergedHistory.length; i++) {
+        const prevElement = i > 0 ? unmergedHistory[i - 1] : undefined;
+        const element = unmergedHistory[i];
+
+        // user must be defined
+        if (element.user) {
+          // if this history item's _id matches the previous item's _id,
+          // AND they have the same action (e.g. patched vs published),
+          // we need to merge them in the merged history array
+          if (element._id === prevElement?._id && element.action === prevElement.action) {
+            const mergedIndex = merged.findIndex((item) => item._id === element._id);
+            const userAlreadyInUsers = !!merged[mergedIndex].users
+              .filter((user) => !!user)
+              .find((user) => user._id === element.user._id);
+            merged[mergedIndex] = {
+              // use the existing matchign data (_id, name, in, etc.)
+              ...merged[mergedIndex],
+              // merge user from the current history item (in the loop) into the users array
+              users: userAlreadyInUsers
+                ? merged[mergedIndex].users
+                : [...merged[mergedIndex].users, element.user],
+              // use the most recent ISO date string
+              at: new Date(element.at) > new Date(merged[mergedIndex].at) ? element.at : merged[mergedIndex].at,
+            };
+          }
+
+          // otherwise, add the element to the mergd history array
+          else {
+            const { user } = element; // destructure so that user is not pushed
+            merged.push({ ...element, users: [user] });
+          }
+        }
+      }
+    }
+
+    return merged;
+  })();
+
+  // determine the largest amount of users in a single merged activity
+  const mostUsers =
+    activity.length > 0
+      ? activity.reduce((max, obj) => (obj.users.length > max.users.length ? obj : max)).users.length
+      : undefined;
+
+  // determine collection locations (to be used for links to cms items)
+  const collectionLocations: Record<string, collectionType<any>['row']> = {};
+  Object.entries(collections).forEach(([key, value]) => {
+    const colNameL = value!.query.name.plural.toLowerCase();
+    if (value?.row) collectionLocations[colNameL] = value.row;
+    else {
+      collectionLocations[colNameL] = {
+        href: value!.query.name.plural,
+        hrefSuffixKey: '_id',
+      };
+    }
+  });
 
   return (
     <>
       {
-        // generate lines for each history item
-        itemHistory?.slice(0, 6).map((item, index: number) => {
-          let { user, type, at } = item.history[item.history.length - 1];
-
-          const userName = profiles?.find((profile) => profile.github_id === user)?.name;
+        // generate lines for each activity item
+        activity?.slice(0, 6).map((item, index: number) => {
+          let type = item.action;
+          const at = item.at;
+          const users = item.users;
 
           // rename history types
           type =
@@ -48,28 +113,150 @@ function RecentActivity() {
               ? 'removed'
               : 'modified';
 
+          // construct the link to the collection item page
+          let locHref = '';
+          const colLoc = collectionLocations[item.in];
+          if (colLoc) {
+            const { href, hrefSuffixKey, hrefSearch } = colLoc;
+            if (item.in === 'photos') locHref = `/cms/photos/library/${item._id}`;
+            else if (hrefSuffixKey === '_id') locHref = `${href}/${item._id}${hrefSearch || ''}`;
+            else locHref = `${href.replace('item', 'collection')}`;
+          }
+
           return (
             <ItemWrapper theme={theme} key={index}>
               <Item>
-                <Profile
-                  theme={theme}
-                  src={`${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_BASE_URL}/api/v2/users/${user}/photo`}
+                <ListNames
+                  NameComponent={Bold}
+                  PhotoComponent={ProfilePhoto}
+                  TextWrapperComponent={Text}
+                  names={users.map((user) => ({
+                    name: user.name,
+                    onClick: () => history.push(`/profile/${user._id}`),
+                    photo: `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_BASE_URL}/v3/user-photo/${user._id}`,
+                  }))}
+                  appendText={
+                    <>
+                      <span> {type} </span>
+                      <Bold theme={theme} onClick={() => history.push(locHref)}>
+                        {item.name}
+                      </Bold>
+                      <span> in </span>
+                      <span>{item.in}</span>
+                    </>
+                  }
+                  photos={{
+                    areShown: true,
+                    maxPhotos: mostUsers && mostUsers > 3 ? 3 : mostUsers,
+                    photoWidth: 24,
+                  }}
                 />
-                <Text>
-                  <Bold onClick={() => history.push(`/profile/${user}`)}>{userName || user}</Bold>
-                  <span> {type} </span>
-                  <Bold onClick={() => history.push(`/cms/item/${item.collection}/${item._id}`)}>
-                    {item.name}
-                  </Bold>
-                  <span> in </span>
-                  <span>{item.collection}</span>
-                </Text>
               </Item>
-              <Date theme={theme}>{DateTime.fromISO(at).toRelative()}</Date>
+              <DateComponent theme={theme}>{DateTime.fromISO(at).toRelative()}</DateComponent>
             </ItemWrapper>
           );
         })
       }
+    </>
+  );
+}
+
+interface IListNames {
+  NameComponent: React.ComponentType<{ onClick?: MouseEventHandler<HTMLElement>; theme: themeType }>;
+  PhotoComponent: React.ComponentType<{
+    onClick?: MouseEventHandler<HTMLElement>;
+    theme: themeType;
+    src?: string;
+  }>;
+  TextWrapperComponent: React.ComponentType;
+  names: Array<{
+    name: string;
+    onClick?: MouseEventHandler<HTMLElement>;
+    photo?: string;
+  }>;
+  appendText?: React.ReactNode;
+  photos?: {
+    areShown?: boolean; // default true
+    maxPhotos?: number; // default 3
+    photoWidth: number;
+  };
+}
+
+function ListNames(props: IListNames) {
+  const theme = useTheme() as themeType;
+  const colorHash = new ColorHash({ saturation: 0.8, lightness: 0.5 });
+
+  // shows a list of photos that overlap each other
+  // automatically sets z-index to ensure first photo is on top and last photo is on bottom
+  const PhotosWrapper = styled.div<{ maxPhotos?: number; photoWidth: number; ids: string[] }>`
+    display: grid;
+    grid-template-columns: ${({ maxPhotos, photoWidth }) =>
+      Array.from(Array((maxPhotos || 3) - 1))
+        .map(() => `16px`)
+        .join(` `) + ` ${photoWidth}px`};
+    ${({ maxPhotos, ids }) =>
+      Array.from(Array(maxPhotos || 3))
+        .map(
+          (_, index, arr) => `
+    > *:nth-of-type(${index + 1}) {
+      z-index: ${arr.length - index};
+      box-shadow: inset 0 0 0 1.5px ${colorHash.hex(
+        ids[index] || Math.random().toString()
+      )}, 0 0 2px 0.5px #00000040;
+    }
+    `
+        )
+        .join('\n')};
+  `;
+
+  return (
+    <>
+      {props.photos ? (
+        <PhotosWrapper
+          maxPhotos={props.photos.maxPhotos}
+          photoWidth={props.photos.photoWidth}
+          ids={props.names.map((n) => n.name)}
+        >
+          {props.names.slice(0, props.photos.maxPhotos || 3).map(({ photo, onClick }) => {
+            return <props.PhotoComponent src={photo} theme={theme} onClick={onClick} />;
+          })}
+        </PhotosWrapper>
+      ) : null}
+      <props.TextWrapperComponent>
+        {props.names.length === 1 ? (
+          <props.NameComponent theme={theme} onClick={props.names[0].onClick}>
+            <span>{props.names[0].name}</span>
+          </props.NameComponent>
+        ) : props.names.length === 2 ? (
+          <>
+            <props.NameComponent theme={theme} onClick={props.names[0].onClick}>
+              <span>{props.names[0].name}</span>
+            </props.NameComponent>
+            <span> and </span>
+            <props.NameComponent theme={theme} onClick={props.names[1].onClick}>
+              <span>{props.names[1].name}</span>
+            </props.NameComponent>
+          </>
+        ) : props.names.length >= 3 ? (
+          <>
+            {props.names.slice(0, props.names.length - 1).map((name, index: number) => {
+              return (
+                <>
+                  <props.NameComponent theme={theme} onClick={name.onClick}>
+                    <span key={index}>{name.name}</span>
+                  </props.NameComponent>
+                  <span>, </span>
+                </>
+              );
+            })}
+            <span>and </span>
+            <props.NameComponent theme={theme} onClick={props.names.slice(-1)[0].onClick}>
+              <span>{props.names.slice(-1)[0].name}</span>
+            </props.NameComponent>
+          </>
+        ) : null}
+        {props.appendText}
+      </props.TextWrapperComponent>
     </>
   );
 }
@@ -100,13 +287,13 @@ const Text = styled.div`
   margin: 0 8px;
 `;
 
-const Date = styled.div<{ theme: themeType }>`
+const DateComponent = styled.div<{ theme: themeType }>`
   flex-shrink: 0;
   font-size: 11px;
   color: ${({ theme }) => theme.color.neutral[theme.mode][900]};
 `;
 
-const Bold = styled.a`
+const Bold = styled.a<{ theme: themeType }>`
   font-weight: 700;
   cursor: pointer;
   &:hover {
@@ -114,11 +301,15 @@ const Bold = styled.a`
   }
 `;
 
-const Profile = styled.img<{ theme: themeType }>`
+const ProfilePhoto = styled.div<{ theme: themeType; src?: string }>`
   width: 24px;
   height: 24px;
   flex-shrink: 0;
-  border-radius: ${({ theme }) => theme.radius};
+  border-radius: 50%;
+  background: ${({ src }) => `url(${src})`};
+  background-position: center;
+  background-size: cover;
+  box-shadow: inset 0 0 0 1.5px black;
 `;
 
 export { RecentActivity };
