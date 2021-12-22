75dbaa21c6f7ee62c435c419f88fbac930720351
Jack Buehner <github@jackbuehner.com>
2021-12-22 03:21:14 +0000

    feat: use new userExists schema

--- src/graphql/queries/USER_EXISTS.ts
+++ src/graphql/queries/USER_EXISTS.ts
@@ -1,18 +1,30 @@
 import { gql } from '@apollo/client';
-import { jsonToGraphQLQuery, VariableType } from 'json-to-graphql-query';
 
 /**
  * Gets whether a user exists by username
  */
 const USER_EXISTS = gql`
   query ($username: String!) {
-    userExists(username: $username)
+    userExists(username: $username) {
+      exists
+      doc {
+        name
+        email
+      }
+    }
   }
 `;
 
 type USER_EXISTS__TYPE =
   | {
-      userExists: boolean;
+      userExists: {
+        exists: boolean;
+        doc?: {
+          name: string;
+          email: string;
+          slug: string;
+        };
+      };
     }
   | undefined;
 
