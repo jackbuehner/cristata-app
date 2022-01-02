ee8a4870c1dafdb890beeef14cf17c0b7001c784
Jack Buehner <github@jackbuehner.com>
2022-01-01 20:58:59 -0500

    feat: add Spinner component

---
+++ src/components/Loading/Spinner.tsx
@@ -0,0 +1,29 @@
+import { css, SerializedStyles, useTheme } from '@emotion/react';
+import styled, { CSSObject } from '@emotion/styled/macro';
+import { CircularProgress } from '@material-ui/core';
+import { colorType, themeType } from '../../utils/theme/theme';
+
+const Component = styled(CircularProgress)<{
+  theme: themeType;
+  col: colorType;
+  size: number;
+  style?: SerializedStyles;
+}>`
+  width: ${({ size }) => size}px !important;
+  height: ${({ size }) => size}px !important;
+  color: ${({ theme, col }) =>
+    col === 'neutral' ? theme.color.neutral[theme.mode][900] : theme.color[col][900]} !important;
+  ${({ style }) => style}
+`;
+
+interface ISpinner {
+  size?: number;
+  style?: CSSObject;
+}
+
+function Spinner(props: ISpinner) {
+  const theme = useTheme() as themeType;
+  return <Component theme={theme} col={'primary'} size={props.size || 20} style={css(props.style)} />;
+}
+
+export { Spinner };
