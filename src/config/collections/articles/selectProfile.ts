7d0c72c4382e5fd8c8b1f8620cf444320209a598
Jack Buehner <github@jackbuehner.com>
2022-01-05 14:24:59 -0500

    feat: show user status and slug in profile select

--- src/config/collections/articles/selectProfile.ts
+++ src/config/collections/articles/selectProfile.ts
@@ -2,11 +2,18 @@ import { ApolloClient, gql, NormalizedCacheObject } from '@apollo/client';
 import { jsonToGraphQLQuery } from 'json-to-graphql-query';
 import mongoose from 'mongoose';
 import { Paged } from '../../../interfaces/cristata/paged';
+import { getPasswordStatus } from '../../../utils/axios/getPasswordStatus';
 import { isObjectId } from '../../../utils/isObjectId';
 
 async function selectProfile(inputValue: string, client: ApolloClient<NormalizedCacheObject>) {
   // get the five years that best match the current input
-  type QueryDocType = { _id: mongoose.Types.ObjectId; name: string };
+  type QueryDocType = {
+    _id: mongoose.Types.ObjectId;
+    name: string;
+    retired?: boolean;
+    flags: string[];
+    slug: string;
+  };
   const QUERY = (input: string) =>
     gql(
       jsonToGraphQLQuery({
@@ -21,6 +28,9 @@ async function selectProfile(inputValue: string, client: ApolloClient<Normalized
             docs: {
               _id: true,
               name: true,
+              slug: true,
+              retired: true,
+              flags: true,
             },
           },
         },
@@ -32,10 +42,23 @@ async function selectProfile(inputValue: string, client: ApolloClient<Normalized
   });
 
   // with the user data, create the options array
-  const options = data.users.docs.map((user) => ({
-    value: new mongoose.Types.ObjectId(user._id).toHexString(),
-    label: user.name,
-  }));
+  const options = data.users.docs.map((user) => {
+    const { temporary, expired } = getPasswordStatus(user.flags);
+
+    return {
+      value: new mongoose.Types.ObjectId(user._id).toHexString(),
+      label:
+        user.name +
+        ` (${user.slug})` +
+        (user.retired
+          ? ' [DEACTIVATED/RETIRED]'
+          : expired
+          ? ' [INVITE IGNORED]'
+          : temporary
+          ? ' [PENDING INVITE]'
+          : ''),
+    };
+  });
 
   // return the opions array
   return options;
