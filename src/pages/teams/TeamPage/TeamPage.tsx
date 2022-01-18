9fcba9128f974c8a7edf3e6d447f76186d543ae0
Jack Buehner <github@jackbuehner.com>
2022-01-18 00:15:14 -0500

    fix(TeamPage): error when user is undefined or null
    This commit filters out undefined or null users from the members and organizers arrays.

--- src/pages/teams/TeamPage/TeamPage.tsx
+++ src/pages/teams/TeamPage/TeamPage.tsx
@@ -95,7 +95,8 @@ function TeamPage() {
   const canManage =
     permissions?.modify ||
     team?.organizers
-      ?.map((user) => user._id)
+      ?.filter((m) => !!m)
+      .map((user) => user._id)
       .includes(JSON.parse(localStorage.getItem('auth.user') as string)?._id) ||
     false;
   const canDeactivate = permissionsData?.userActionAccess?.deactivate || false;
@@ -135,7 +136,8 @@ function TeamPage() {
     const canManage =
       permissions?.modify ||
       data?.team.organizers
-        ?.map((user) => user._id)
+        ?.filter((m) => !!m)
+        .map((user) => user._id)
         .includes(JSON.parse(localStorage.getItem('auth.user') as string)?._id) ||
       false;
 
@@ -383,8 +385,11 @@ function TeamPage() {
             variables: {
               _id: team_id,
               input: {
-                organizers: team.organizers.filter((user) => user._id !== _id).map((user) => user._id), // remove user from organizers
-                members: Array.from(new Set([...team.members.map((user) => user._id), _id])),
+                organizers: team.organizers
+                  .filter((m) => !!m)
+                  .filter((user) => user._id !== _id)
+                  .map((user) => user._id), // remove user from organizers
+                members: Array.from(new Set([...team.members.filter((m) => !!m).map((user) => user._id), _id])),
               },
             },
           })
@@ -407,8 +412,13 @@ function TeamPage() {
             variables: {
               _id: team_id,
               input: {
-                organizers: Array.from(new Set([...team.organizers.map((user) => user._id), _id])),
-                members: team.members.filter((user) => user._id !== _id).map((user) => user._id), // remove user from members
+                organizers: Array.from(
+                  new Set([...team.organizers.filter((m) => !!m).map((user) => user._id), _id])
+                ),
+                members: team.members
+                  .filter((m) => !!m)
+                  .filter((user) => user._id !== _id)
+                  .map((user) => user._id), // remove user from members
               },
             },
           })
@@ -544,8 +554,14 @@ function TeamPage() {
           variables: {
             _id: team_id,
             input: {
-              organizers: team?.organizers.filter((user) => user._id !== userId).map((user) => user._id), // remove user from organizers
-              members: team?.members.filter((user) => user._id !== userId).map((user) => user._id), // remove user from organizers
+              organizers: team?.organizers
+                .filter((m) => !!m)
+                .filter((user) => user._id !== userId)
+                .map((user) => user._id), // remove user from organizers
+              members: team?.members
+                .filter((m) => !!m)
+                .filter((user) => user._id !== userId)
+                .map((user) => user._id), // remove user from organizers
             },
           },
         })
@@ -648,7 +664,8 @@ function TeamPage() {
           <ContentWrapper theme={theme} key={0}>
             <UsersGrid>
               {allMembers.map((user, index) => {
-                const isOrganizer = team?.organizers.findIndex((u) => u._id === user._id) !== -1;
+                const isOrganizer =
+                  team?.organizers.filter((m) => !!m).findIndex((u) => u._id === user._id) !== -1;
                 const { temporary, expired } = getPasswordStatus(user.flags);
 
                 return (
