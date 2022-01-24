67cf774db1e7dc4774aa5bab43be16f9e70e4032
Jack Buehner <github@jackbuehner.com>
2022-01-24 16:37:37 +0000

    refactor(ToastContainer): move toast container to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ import styled from '@emotion/styled/macro';
 import useAxios, { configure } from 'axios-hooks';
 import { SideNavSubButton } from './components/Button';
 import { PlansSideNavSub } from './pages/plans/PlansSideNavSub';
-import { Dismiss16Regular } from '@fluentui/react-icons';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { CristataWebSocket } from './components/CristataWebSocket/CristataWebSocket';
 import { ItemDetailsPage } from './pages/CMS/ItemDetailsPage';
@@ -22,7 +20,6 @@ import { PhotoLibraryPage } from './pages/CMS/PhotoLibraryPage';
 import { themeType } from './utils/theme/theme';
 import { SidenavSub } from './components/SidenavSub';
 import { Sidenav } from './components/Sidenav/Sidenav';
-import Color from 'color';
 import { HomePage } from './pages/Home';
 import { getNavigationConfig } from './config';
 import { Titlebar } from './components/Titlebar';
@@ -34,6 +31,7 @@ import { TeamsNav } from './pages/teams/TeamsNav';
 import { TeamPage } from './pages/teams/TeamPage';
 import { PageHead } from './components/PageHead';
 import { useAppSelector } from './redux/hooks';
+import { ToastContainer } from './components/ToastContainer';
 
 // configure axios global settings
 configure({ axios: db });
@@ -71,69 +69,12 @@ function App() {
   // store whether the nav is shown
   const [isNavVisibleM, setIsNavVisibleM] = useState(false);
 
-  const StyledToastContainer = styled(ToastContainer)<{
-    appTheme: themeType;
-    icon?: false | React.ReactNode;
-  }>`
-    top: ${({ appTheme }) => `calc(${appTheme.dimensions.PageHead.height} + 6px)`};
-    .Toastify__toast {
-      border-radius: ${({ appTheme }) => appTheme.radius};
-      padding: 0;
-      background-color: ${({ appTheme }) => (appTheme.mode === 'light' ? 'white' : 'black')};
-      color: ${({ appTheme }) => appTheme.color.neutral[appTheme.mode][1400]};
-      font-family: ${({ appTheme }) => appTheme.font.detail};
-      font-size: 15px;
-      &::before {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 50px;
-        background-color: ${({ appTheme }) => (appTheme.mode === 'light' ? 'white' : 'black')};
-      }
-    }
-    .Toastify__toast--error {
-      &::before {
-        content: '❌';
-        border-left: ${({ appTheme }) => `3px solid ${appTheme.color.danger[800]}`};
-      }
-    }
-    .Toastify__toast--warning {
-      &::before {
-        content: '❗';
-        border-left: ${({ appTheme }) => `3px solid ${appTheme.color.orange[800]}`};
-      }
-    }
-    .Toastify__toast--success {
-      &::before {
-        content: '✅';
-        border-left: ${({ appTheme }) => `3px solid ${appTheme.color.success[800]}`};
-      }
-    }
-    .Toastify__toast-body {
-      width: 100%;
-      padding-left: 0;
-    }
-    .Toastify__progress-bar {
-      background-color: ${({ appTheme }) =>
-        Color(appTheme.color.neutral[appTheme.mode][800]).alpha(0.25).string()};
-      height: 3px;
-    }
-  `;
-
   //@ts-expect-error windowControlsOverlay is only available in some browsers
   const isCustomTitlebarVisible = navigator.windowControlsOverlay?.visible;
 
   return (
     <>
-      <StyledToastContainer
-        appTheme={theme}
-        icon={false}
-        closeButton={
-          <span style={{ margin: '8px 8px 0 0' }}>
-            <Dismiss16Regular />
-          </span>
-        }
-      />
+      <ToastContainer />
       <Router>
         <SplashScreen
           loading={loadingUser}
---
+++ src/components/ToastContainer/ToastContainer.tsx
@@ -0,0 +1,73 @@
+import styled from '@emotion/styled/macro';
+import Color from 'color';
+import { ToastContainer as ReactToastContainer } from 'react-toastify';
+import { themeType } from '../../utils/theme/theme';
+import { useTheme } from '@emotion/react';
+import { Dismiss16Regular } from '@fluentui/react-icons';
+
+function ToastContainer() {
+  const theme = useTheme() as themeType;
+
+  return (
+    <StyledToastContainer
+      appTheme={theme}
+      icon={false}
+      closeButton={
+        <span style={{ margin: '8px 8px 0 0' }}>
+          <Dismiss16Regular />
+        </span>
+      }
+    />
+  );
+}
+
+const StyledToastContainer = styled(ReactToastContainer)<{
+  appTheme: themeType;
+  icon?: false | React.ReactNode;
+}>`
+  top: ${({ appTheme }) => `calc(${appTheme.dimensions.PageHead.height} + 6px)`};
+  .Toastify__toast {
+    border-radius: ${({ appTheme }) => appTheme.radius};
+    padding: 0;
+    background-color: ${({ appTheme }) => (appTheme.mode === 'light' ? 'white' : 'black')};
+    color: ${({ appTheme }) => appTheme.color.neutral[appTheme.mode][1400]};
+    font-family: ${({ appTheme }) => appTheme.font.detail};
+    font-size: 15px;
+    &::before {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 50px;
+      background-color: ${({ appTheme }) => (appTheme.mode === 'light' ? 'white' : 'black')};
+    }
+  }
+  .Toastify__toast--error {
+    &::before {
+      content: '❌';
+      border-left: ${({ appTheme }) => `3px solid ${appTheme.color.danger[800]}`};
+    }
+  }
+  .Toastify__toast--warning {
+    &::before {
+      content: '❗';
+      border-left: ${({ appTheme }) => `3px solid ${appTheme.color.orange[800]}`};
+    }
+  }
+  .Toastify__toast--success {
+    &::before {
+      content: '✅';
+      border-left: ${({ appTheme }) => `3px solid ${appTheme.color.success[800]}`};
+    }
+  }
+  .Toastify__toast-body {
+    width: 100%;
+    padding-left: 0;
+  }
+  .Toastify__progress-bar {
+    background-color: ${({ appTheme }) =>
+      Color(appTheme.color.neutral[appTheme.mode][800]).alpha(0.25).string()};
+    height: 3px;
+  }
+`;
+
+export { ToastContainer };
