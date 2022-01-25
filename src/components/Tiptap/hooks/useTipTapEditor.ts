bf6f0fe2c9c57577df3d2b1fa579618c4eec6329
Jack Buehner <github@jackbuehner.com>
2022-01-25 05:30:44 +0000

    feat(tiptap): debounce forced updates
    This tiptap editor hook increments a counter on every editor transaction. This causes every component that relies on the editor to re-render. This is important for ensuring the editor is in sync with presence indicators and decorations.
    However, re-rendering can be expensive. If the writer performs rapid changes such as holding the backspace key or typing extremely fast, the app can hang while it attempts to work its ways through all of the re-renders.
    This commit debounces the counter increment by 30ms to ensure the editor does not hang.

--- src/components/Tiptap/hooks/useTipTapEditor.ts
+++ src/components/Tiptap/hooks/useTipTapEditor.ts
@@ -1,11 +1,18 @@
-import { useCallback, useState, useEffect, useRef } from 'react';
 import { EditorOptions } from '@tiptap/core';
 import { Editor } from '@tiptap/react';
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
+import { useEffect, useRef, useState } from 'react';
 
 function useForceUpdate() {
   const [, setValue] = useState(0);
 
-  return useCallback(() => setValue((value) => value + 1), []);
+  // debounce the forced update by 30ms to avoid expensive state updates
+  // when user does rapid changes such as holding the backspace key
+  const increment = AwesomeDebouncePromise(() => {
+    setValue((value) => value + 1);
+  }, 30);
+
+  return increment;
 }
 
 const useTipTapEditor = (options: Partial<EditorOptions> = {}): Editor | null => {
