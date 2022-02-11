169640dea9ceb8e993800a9ed0248a83b38d5ae5
Jack Buehner <github@jackbuehner.com>
2022-02-11 23:53:51 +0000

    feat: automatic signin for fathom

--- src/pages/embeds/FathomEmbed.tsx
+++ src/pages/embeds/FathomEmbed.tsx
@@ -1,6 +1,8 @@
-import { Open24Regular } from '@fluentui/react-icons';
-import { Dispatch, SetStateAction, useEffect } from 'react';
+import { ArrowClockwise16Regular, Open24Regular } from '@fluentui/react-icons';
+import useAxios from 'axios-hooks';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { IconButton } from '../../components/Button';
+import { Spinner } from '../../components/Loading';
 import { PageHead } from '../../components/PageHead';
 
 interface IFathomEmbed {
@@ -17,9 +19,18 @@ interface IFathomEmbed {
 }
 
 function FathomEmbed(props: IFathomEmbed) {
+  const { gridCols, setGridCols } = props;
+  const [count, setCount] = useState<number>(0);
+
   useEffect(() => {
-    props.setGridCols({ ...props.gridCols, sideSub: 0 });
-  });
+    if (gridCols.sideSub > 0) {
+      setGridCols({ ...gridCols, sideSub: 0 });
+    }
+  }, [gridCols, setGridCols]);
+
+  const [{ data }] = useAxios<{ url: string }>(
+    `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_BASE_URL}/analytics/dashboard`
+  );
 
   return (
     <div style={{ overflow: 'hidden', height: '100%' }}>
@@ -27,20 +38,42 @@ function FathomEmbed(props: IFathomEmbed) {
         title={'Fathom Analytics'}
         description={'thepaladin.news'}
         buttons={
-          <>
-            <IconButton
-              onClick={() => window.open('https://app.usefathom.com/share/uiipkyhl/thepaladin.news')}
-              icon={<Open24Regular />}
-            >
-              Open Externally
-            </IconButton>
-          </>
+          data?.url ? (
+            <>
+              <IconButton onClick={() => setCount((count) => count + 1)} icon={<ArrowClockwise16Regular />}>
+                Refresh frame
+              </IconButton>
+              <IconButton
+                onClick={() => window.open(data?.url, 'fathom', 'width=1200,height=800')}
+                icon={<Open24Regular />}
+              >
+                Open Externally
+              </IconButton>
+            </>
+          ) : (
+            <></>
+          )
         }
       />
+      <div
+        style={{
+          width: 'calc(100% - 80px)',
+          height: 'calc(100% - 64px)',
+          border: 'none',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          position: 'fixed',
+          zIndex: 1,
+        }}
+      >
+        <Spinner color={'neutral'} colorShade={1500} size={30} />
+      </div>
       <iframe
+        key={count}
         title={'fathom'}
-        src={'https://app.usefathom.com/share/uiipkyhl/thepaladin.news'}
-        style={{ width: '100%', height: 'calc(100% - 64px)', border: 'none' }}
+        src={data?.url}
+        style={{ width: '100%', height: 'calc(100% - 64px)', border: 'none', zIndex: 1, position: 'relative' }}
       />
     </div>
   );
