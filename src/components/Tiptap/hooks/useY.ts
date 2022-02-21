4c33f48db105ba0e4928cada6cd2f78ccc44a6e2
Jack Buehner <github@jackbuehner.com>
2022-02-21 00:14:33 +0000

    fix: wait for sync instead of update
    Documents that are empty do not receive updates.

--- src/components/Tiptap/hooks/useY.ts
+++ src/components/Tiptap/hooks/useY.ts
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { DependencyList, useMemo, useState } from 'react';
+import { DependencyList, useMemo } from 'react';
 import { WebsocketProvider } from 'y-websocket';
 import * as Y from 'yjs';
 import packageJson from '../../../../package.json';
@@ -24,17 +24,11 @@ function useY({ name, ws }: UseYProps, deps: DependencyList = []): ReturnType {
     [ws, name, doc, params, ...deps]
   );
 
-  // check that the doc has received the server data first
-  const [updated, setUpdated] = useState<boolean>(false);
-  doc.once('update', () => {
-    if (!updated) setUpdated(true);
-  });
-
   // whether the doc is connected to the server and is up-to-date
   let isConnected = undefined;
   if (hocuspocus.wsconnecting) isConnected = undefined;
-  else if (hocuspocus.wsconnected && !updated) isConnected = undefined;
-  else if (hocuspocus.wsconnected && updated) isConnected = true;
+  else if (hocuspocus.wsconnected && !hocuspocus.synced) isConnected = undefined;
+  else if (hocuspocus.wsconnected && hocuspocus.synced) isConnected = true;
   else isConnected = false;
 
   return [doc, settingsMap, hocuspocus, isConnected];
