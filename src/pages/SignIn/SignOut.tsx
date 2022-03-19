99890b5dc2f0dc23a87e366410d5b16d7eb431ab
Jack Buehner <github@jackbuehner.com>
2022-03-19 22:24:09 +0000

    feat: sign out via POST instead of redirecting to server

--- src/pages/SignIn/SignOut.tsx
+++ src/pages/SignIn/SignOut.tsx
@@ -1,5 +1,21 @@
+import { toast } from 'react-toastify';
+
 function SignOut() {
-  window.location.href = `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_BASE_URL}/auth/clear`;
+  fetch(`${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_BASE_URL}/auth/clear`, {
+    method: 'post',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-cache',
+  })
+    .catch(() => {
+      toast.error('Failed to sign out');
+    })
+    .finally(() => {
+      window.location.href = window.location.protocol + '//' + window.location.host + process.env.PUBLIC_URL;
+    });
+
   return null;
 }
 
