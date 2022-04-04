ea2e94ace6c18930622c58455819cf29d0fd21e9
Jack Buehner <github@jackbuehner.com>
2022-04-04 08:32:07 +0000

    fix: include logic that works when the app is compiled

--- src/utils/isFluentIconComponent.ts
+++ src/utils/isFluentIconComponent.ts
@@ -1,8 +1,9 @@
 function isFluentIconComponent(toCheck: unknown): toCheck is React.ComponentType {
   return (
-    typeof toCheck === 'function' &&
-    (String(toCheck).includes('return react') || String(toCheck).includes('return React')) &&
-    String(toCheck).includes('createElement')
+    (typeof toCheck === 'function' &&
+      (String(toCheck).includes('return react') || String(toCheck).includes('return React')) &&
+      String(toCheck).includes('createElement')) ||
+    String(toCheck).includes('.createElement("svg"')
   );
 }
 
