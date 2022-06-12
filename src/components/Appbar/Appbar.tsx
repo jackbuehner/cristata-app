dfac99e61c647e33344abf30492bc6fc65744276
Jack Buehner <github@jackbuehner.com>
2022-06-12 23:38:08 +0000

    feat: add a return to dashboard button when mouse over app icon

--- src/components/Appbar/Appbar.tsx
+++ src/components/Appbar/Appbar.tsx
@@ -1,6 +1,11 @@
-import { AppGeneric20Regular } from '@fluentui/react-icons';
-import { CSSProperties } from 'react';
+import { useTheme } from '@emotion/react';
+import { AppGeneric20Regular, ArrowLeft24Regular } from '@fluentui/react-icons';
+import Color from 'color';
+import { CSSProperties, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../redux/hooks';
+import { themeType } from '../../utils/theme/theme';
+import { Button } from '../Button';
 import {
   AppbarAccountMenu as AccountMenu,
   AppbarActions as Actions,
@@ -11,6 +16,11 @@ import {
 
 function Appbar() {
   const state = useAppSelector(({ appbar }) => appbar);
+  const theme = useTheme() as themeType;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [showHomeArrow, setShowHomeArrow] = useState(false);
 
   const left: CSSProperties = {
     flexGrow: 1,
@@ -32,7 +42,38 @@ function Appbar() {
   return (
     <Bar loading={state.loading}>
       <div style={left}>
-        <Icon icon={state.icon || AppGeneric20Regular} color={state.color} name={state.name} />
+        {location.pathname !== '/' ? (
+          <div
+            onMouseEnter={() => setShowHomeArrow(true)}
+            onMouseLeave={() => setShowHomeArrow(false)}
+            style={{
+              padding: '9px',
+              position: 'absolute',
+              zIndex: 1,
+              backgroundColor: Color(theme.color.neutral[theme.mode][300]).alpha(0.3).string(),
+              backdropFilter: 'blur(10px)',
+              transform: `translateX(${showHomeArrow ? 0 : -200}px)`,
+              opacity: showHomeArrow ? 1 : 0,
+              transition: `all 240ms ${!showHomeArrow ? `ease` : `cubic-bezier(0.1, 0.9, 0.2, 1)`}`,
+            }}
+          >
+            <Button
+              icon={<ArrowLeft24Regular />}
+              onClick={() => {
+                navigate('/');
+                setShowHomeArrow(false);
+              }}
+            >
+              Return to dashboard
+            </Button>
+          </div>
+        ) : null}
+        <Icon
+          icon={state.icon || AppGeneric20Regular}
+          color={state.color}
+          name={state.name}
+          onMouseEnter={() => setShowHomeArrow(true)}
+        />
         <Name>{state.name}</Name>
       </div>
       <div style={right}>
