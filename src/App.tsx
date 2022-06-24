8af057bd0868f6cae77b25d2c6aa68224e67f887
Jack Buehner <github@jackbuehner.com>
2022-06-24 04:25:37 +0000

    fix: supress errors that do not matter

--- src/App.tsx
+++ src/App.tsx
@@ -74,6 +74,19 @@ function App() {
     }
   }, [tenant]);
 
+  // suppress warnings that we do not care about
+  const consoleError = console.error;
+  console.error = function filterWarnings(msg) {
+    const supressedWarnings = [
+      'prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase',
+      '`label` of `value` is not same as `label` in Select options.',
+    ];
+
+    if (!supressedWarnings.some((entry) => msg.includes(entry))) {
+      consoleError.apply(console, arguments as unknown as any[]);
+    }
+  };
+
   return (
     <ApolloProvider client={client}>
       <ReduxProvider store={store}>
