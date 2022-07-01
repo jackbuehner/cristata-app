363aba317012327157f770854272ea36c1d461de
Jack Buehner <github@jackbuehner.com>
2022-07-01 02:53:12 +0000

    fix: overwrite emotion theme type to match themType type
    We not longer need to overwrite the type every time we have a styled component, which means we also no longer need to pass a theme prop to styled components (emotion does it automatically). useTheme() is also not typed correctly.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,6 @@
+import '@emotion/react';
+import { themeType } from './utils/theme/theme';
+
 declare module 'react-use-dimensions' {
   var useDimensions: any;
   export = useDimensions;
@@ -6,4 +9,8 @@ declare module 'react-use-dimensions' {
 declare module 'graphiql-explorer' {
   var GraphiQLExplorer: any;
   export = GraphiQLExplorer;
+}
+
+declare module '@emotion/react' {
+  export interface Theme extends themeType {}
 }
