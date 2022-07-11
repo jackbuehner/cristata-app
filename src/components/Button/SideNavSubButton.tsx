5864bf861c16b56c3badf0032193bb54303c6608
Jack Buehner <github@jackbuehner.com>
2022-07-11 00:35:54 +0000

    feat: show collection scopes as tag to right of name

--- src/components/Button/SideNavSubButton.tsx
+++ src/components/Button/SideNavSubButton.tsx
@@ -1,10 +1,11 @@
 /** @jsxImportSource @emotion/react */
+import styled from '@emotion/styled/macro';
 import { css, useTheme } from '@emotion/react';
 import Color from 'color';
 import { Dispatch, ReactText, SetStateAction } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Button } from '.';
-import { themeType } from '../../utils/theme/theme';
+import { colorType, themeType } from '../../utils/theme/theme';
 
 function SideNavSubButton(props: {
   children: ReactText;
@@ -20,6 +21,8 @@ function SideNavSubButton(props: {
   const isSameSearch =
     (props.to && location.search === `?${props.to.split('?')[1]}`) ||
     (props.to && location.search === `` && props.to.split('?')[1] === undefined);
+
+  const isScoped = `${props.children}`.split('::').length === 2;
 
   return (
     <Button
@@ -38,6 +41,7 @@ function SideNavSubButton(props: {
           min-width: 0;
           overflow: hidden;
           text-overflow: ellipsis;
+          width: 100%;
         }
       `}
       colorShade={theme.mode === 'light' ? 600 : 300}
@@ -66,9 +70,46 @@ function SideNavSubButton(props: {
         </span>
       }
     >
-      {props.children}
+      <ButtonChild>
+        {isScoped ? (
+          <>
+            <div>{`${props.children}`.split('::')[1]}</div>
+            <Scope color={'neutral'}>{`${props.children}`.split('::')[0]}</Scope>
+          </>
+        ) : (
+          props.children
+        )}
+      </ButtonChild>
     </Button>
   );
 }
+
+const ButtonChild = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 6px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Scope = styled.div<{ color: colorType }>`
+  padding: 3px 5px;
+  font-family: ${({ theme }) => theme.font.detail};
+  font-size: 0.75rem;
+  font-weight: 500;
+  white-space: nowrap;
+  box-shadow: ${({ theme, color }) =>
+      color === 'neutral'
+        ? Color(theme.color[color][theme.mode][1200]).alpha(0.25).string()
+        : Color(theme.color[color][theme.mode === 'light' ? 800 : 300])
+            .alpha(0.1)
+            .string()}
+    0px 0px 0px 1.25px inset !important;
+  border-radius: ${({ theme }) => theme.radius};
+  color: ${({ theme, color }) =>
+    color === 'neutral'
+      ? Color(theme.color[color][theme.mode][1200]).string()
+      : Color(theme.color[color][theme.mode === 'light' ? 800 : 300]).string()};
+`;
 
 export { SideNavSubButton };
