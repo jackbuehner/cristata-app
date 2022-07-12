dfbdeb4bbb2a56fe8946b5dc8ae97d6aa7e48c9f
Jack Buehner <github@jackbuehner.com>
2022-07-12 01:02:17 +0000

    feat: add icons to config nav

--- src/components/FluentIcon/index.ts
+++ src/components/FluentIcon/index.ts
@@ -1,7 +1,7 @@
 import loadable from '@loadable/component';
 import FluentIconsFontCodes from '@fluentui/react-icons/lib/utils/fonts/FluentSystemIcons-Regular.json';
 
-type FluentIconNames = keyof typeof FluentIconsFontCodes;
+type FluentIconNames = keyof typeof FluentIconsFontCodes | 'CircleSmall20Filled';
 
 const FluentIcon = loadable(
   (props: { name: FluentIconNames }) => import(/* webpackChunkName: "FluentIcon" */ `@fluentui/react-icons`),
--- src/pages/configuration/ConfigurationNavigation/ConfigurationNavigation.tsx
+++ src/pages/configuration/ConfigurationNavigation/ConfigurationNavigation.tsx
@@ -1,4 +1,5 @@
 import { SideNavSubButton } from '../../../components/Button';
+import FluentIcon from '../../../components/FluentIcon';
 import { SideNavHeading } from '../../../components/Heading';
 import { Offline } from '../../../components/Offline';
 import { useGetCollections } from './useGetCollections';
@@ -9,14 +10,20 @@ function ConfigurationNavigation() {
   return (
     <>
       <SideNavHeading className={'not-header'}>Billing</SideNavHeading>
-      <SideNavSubButton Icon={<></>} to={`/configuration/billing/usage`}>
+      <SideNavSubButton Icon={<FluentIcon name={'TopSpeed20Regular'} />} to={`/configuration/billing/usage`}>
         Service usage
       </SideNavSubButton>
-      <SideNavSubButton Icon={<></>} to={`/configuration/billing/payments`}>
+      <SideNavSubButton
+        Icon={<FluentIcon name={'ReceiptMoney20Regular'} />}
+        to={`/configuration/billing/payments`}
+      >
         Payments &amp; invoices
       </SideNavSubButton>
       <SideNavHeading>Security</SideNavHeading>
-      <SideNavSubButton Icon={<></>} to={`/configuration/security/tokens-secrets`}>
+      <SideNavSubButton
+        Icon={<FluentIcon name={'Key20Regular'} />}
+        to={`/configuration/security/tokens-secrets`}
+      >
         Secrets
       </SideNavSubButton>
       <SideNavHeading>Schemas</SideNavHeading>
@@ -25,7 +32,11 @@ function ConfigurationNavigation() {
       ) : (
         collections?.map(({ name }, index) => {
           return (
-            <SideNavSubButton key={index} Icon={<></>} to={`/configuration/schema/${name}`}>
+            <SideNavSubButton
+              key={index}
+              Icon={<FluentIcon name={'CircleSmall20Filled'} />}
+              to={`/configuration/schema/${name}`}
+            >
               {name}
             </SideNavSubButton>
           );
