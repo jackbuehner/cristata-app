c27eabaff3eb484ab9959d8d25edbe3249d45273
Jack Buehner <github@jackbuehner.com>
2022-07-16 23:34:05 +0000

    feat: add tabs component

---
+++ src/components/Tabs/Tab.tsx
@@ -0,0 +1,16 @@
+import { Tab as RMWCTab } from '@rmwc/tabs';
+import styled from '@emotion/styled/macro';
+
+const Tab = styled(RMWCTab)`
+  --mdc-theme-primary: ${({ theme }) => theme.color.blue[theme.mode === 'light' ? 800 : 300]};
+
+  .mdc-tab__content .mdc-tab__text-label {
+    font-family: ${({ theme }) => theme.font.detail};
+    text-transform: none;
+    font-weight: 600;
+    color: ${({ theme }) =>
+      theme.mode === 'light' ? theme.color.neutral.light[1200] : theme.color.neutral.dark[1200]};
+  }
+`;
+
+export { Tab };
