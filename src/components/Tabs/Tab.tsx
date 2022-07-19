7523044811fa5631aae9b5a39b57f8f4e31f0db1
Jack Buehner <github@jackbuehner.com>
2022-07-19 05:46:46 +0000

    fix: use primary color for tabs

--- src/components/Tabs/Tab.tsx
+++ src/components/Tabs/Tab.tsx
@@ -2,7 +2,7 @@ import { Tab as RMWCTab } from '@rmwc/tabs';
 import styled from '@emotion/styled/macro';
 
 const Tab = styled(RMWCTab)`
-  --mdc-theme-primary: ${({ theme }) => theme.color.blue[theme.mode === 'light' ? 800 : 300]};
+  --mdc-theme-primary: ${({ theme }) => theme.color.primary[theme.mode === 'light' ? 800 : 300]};
 
   .mdc-tab__content .mdc-tab__text-label {
     font-family: ${({ theme }) => theme.font.detail};
