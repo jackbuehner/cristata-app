12b82a7ebbac261ad32646d4e2cf19f888417314
Jack Buehner <github@jackbuehner.com>
2022-07-20 21:37:48 +0000

    feat: support creating reference fields

--- src/pages/configuration/CollectionSchemaPage/hooks/schema-modals/useCreateSchemaDef.tsx
+++ src/pages/configuration/CollectionSchemaPage/hooks/schema-modals/useCreateSchemaDef.tsx
@@ -3,7 +3,7 @@
 import { useTheme } from '@emotion/react';
 import { DependencyList, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Text } from '../../../../../components/ContentField';
+import { Text, SelectOne } from '../../../../../components/ContentField';
 import { useWindowModal } from '../../../../../hooks/useWindowModal';
 import { useAppSelector } from '../../../../../redux/hooks';
 import { setRootSchemaProperty } from '../../../../../redux/slices/collectionSlice';
@@ -11,6 +11,7 @@ import { slugify } from '../../../../../utils/slugify';
 import { getFieldTypes } from '../../tabs/getFieldTypes';
 import { icons } from '../../tabs/SchemaCard';
 import { EditSchemaDef } from './EditSchemaDef';
+import { useGetCollections } from '../../../ConfigurationNavigation/useGetCollections';
 
 interface UseCreateSchemaDefProps {
   type: keyof typeof icons;
@@ -23,6 +24,7 @@ function useCreateSchemaDef(
   // create the modal
   const [Window, showModal, hideModal] = useWindowModal(() => {
     const theme = useTheme();
+    const [collections] = useGetCollections();
     const state = useAppSelector(({ collectionConfig }) => collectionConfig);
     const dispatch = useDispatch();
 
@@ -45,10 +47,16 @@ function useCreateSchemaDef(
       if (props.type === 'decimal') return 'Float';
       if (props.type === 'number') return 'Number';
       if (props.type === 'objectid') return 'ObjectId';
-      if (props.type === 'reference') return 'String';
+      if (props.type === 'reference') return 'Reference';
       if (props.type === 'richtext') return 'String';
       return 'String';
     })();
+
+    const referenceOptions = (collections || []).map((collection) => ({
+      value: collection.name,
+      label: collection.name,
+    }));
+    const [referenceType, setReferenceType] = useState<string>(referenceOptions[0]?.value || 'User');
 
     return {
       title: !isCreated ? `Create new field` : newName || newId,
@@ -61,15 +69,28 @@ function useCreateSchemaDef(
             onClick: async () => {
               dispatch(setRootSchemaProperty(newId, 'id', newId));
               dispatch(setRootSchemaProperty(newId, 'field.label', newName));
-              dispatch(setRootSchemaProperty(newId, 'type', type));
+              dispatch(setRootSchemaProperty(newId, 'modifiable', true));
+
+              if (type === 'Reference') {
+                dispatch(setRootSchemaProperty(newId, 'type', [referenceType, 'ObjectId']));
+              } else {
+                dispatch(setRootSchemaProperty(newId, 'type', type));
+              }
+
               setIsCreated(true);
               return false;
             },
-            disabled: !newId || !newName || !state.collection || !!idAlreadyExists,
+            disabled:
+              !newId ||
+              !newName ||
+              !state.collection ||
+              !!idAlreadyExists ||
+              (type === 'Reference' && !referenceType),
           }
         : { text: 'Close' },
       children: !isCreated ? (
         <div style={{ padding: '20px 24px' }}>
+          {type}
           <Text
             isEmbedded
             label={'Display name'}
@@ -92,6 +113,21 @@ function useCreateSchemaDef(
               setNewId(slugify(e.currentTarget.value, ''));
             }}
           />
+          {type === 'Reference' ? (
+            <SelectOne
+              isEmbedded
+              label={'Reference collection'}
+              description={
+                'The options for this field will be populated by the documents in the reference collection.'
+              }
+              type={'String'}
+              options={referenceOptions}
+              value={{ label: referenceType, value: referenceType }}
+              onChange={(value) => {
+                if (value && typeof value.value === 'string') setReferenceType(value.value);
+              }}
+            />
+          ) : null}
         </div>
       ) : (
         <EditSchemaDef id={newId} setName={setNewName} />
