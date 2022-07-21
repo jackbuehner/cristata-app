f6ea88e66fd3ecac8f789fcb2147b0290ba4383b
Jack Buehner <github@jackbuehner.com>
2022-07-21 06:02:35 +0000

    feat: add button to create new schema

---
+++ src/pages/configuration/CollectionSchemaPage/hooks/schema-modals/useCreateSchema.tsx
@@ -0,0 +1,97 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { DocumentNode, gql, useApolloClient } from '@apollo/client';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { Text } from '../../../../../components/ContentField';
+import { useWindowModal } from '../../../../../hooks/useWindowModal';
+import { capitalize } from '../../../../../utils/capitalize';
+import { dashToCamelCase } from '../../../../../utils/dashToCamelCase';
+import { slugify } from '../../../../../utils/slugify';
+
+function useCreateSchema(collectionNames: string[]): [React.ReactNode, () => void, () => void] {
+  const [Window, showModal, hideModal] = useWindowModal(() => {
+    const client = useApolloClient();
+    const navigate = useNavigate();
+    const [name, setName] = useState<string>('');
+    const [displayName, setDisplayName] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const nameAlreadyExists = collectionNames.some((colName) => colName === name);
+
+    return {
+      title: `Create new schema`,
+      windowOptions: { name: `createSchema`, width: 370, height: 300 },
+      isLoading: loading,
+      continueButton: {
+        text: 'Create',
+        onClick: async () => {
+          setLoading(true);
+          return await client
+            .mutate<SaveMutationType>({
+              mutation: saveMutationString(name),
+            })
+            .finally(() => {
+              setLoading(false);
+              setName('');
+              setDisplayName('');
+            })
+            .then(({ data }) => {
+              if (data?.setRawConfigurationCollection) {
+                navigate(`/configuration/Schema/${name}#0`);
+                return true;
+              }
+              return false;
+            })
+            .catch((error) => {
+              console.error(error);
+              toast.error(`Failed to save. \n ${error.message}`);
+              return false;
+            });
+        },
+        disabled: nameAlreadyExists,
+      },
+      children: (
+        <>
+          <Text
+            isEmbedded
+            label={'Display name'}
+            description={
+              'The name of the schema as it appears within the Cristata interface. This can be changed later.'
+            }
+            value={displayName}
+            onChange={(e) => {
+              setDisplayName(e.currentTarget.value);
+              setName(capitalize(dashToCamelCase(slugify(e.currentTarget.value))));
+            }}
+          />
+          <Text
+            isEmbedded
+            disabled
+            label={'API name'}
+            description={'The name of the schema in the API. This cannot be changed later.'}
+            value={name}
+            onChange={(e) => setName(capitalize(dashToCamelCase(slugify(e.currentTarget.value))))}
+          />
+          {nameAlreadyExists ? 'Schema name already exists. Choose a unique name.' : null}
+        </>
+      ),
+    };
+  }, []);
+
+  return [Window, showModal, hideModal];
+}
+
+interface SaveMutationType {
+  setRawConfigurationCollection?: string | null;
+}
+
+function saveMutationString(name: string): DocumentNode {
+  return gql`
+    mutation {
+      setRawConfigurationCollection(name: "${name}")
+    }
+  `;
+}
+
+export { useCreateSchema };
--- src/pages/configuration/ConfigurationNavigation/ConfigurationNavigation.tsx
+++ src/pages/configuration/ConfigurationNavigation/ConfigurationNavigation.tsx
@@ -1,51 +1,84 @@
-import { SideNavSubButton } from '../../../components/Button';
+import styled from '@emotion/styled/macro';
+import { Button, SideNavSubButton } from '../../../components/Button';
 import FluentIcon from '../../../components/FluentIcon';
 import { SideNavHeading } from '../../../components/Heading';
 import { Offline } from '../../../components/Offline';
+import { useCreateSchema } from '../CollectionSchemaPage/hooks/schema-modals/useCreateSchema';
 import { useGetCollections } from './useGetCollections';
 
 function ConfigurationNavigation() {
   const [collections] = useGetCollections();
+  const [CreateWindow, showCreateWindow] = useCreateSchema(collections?.map((col) => col.name) || []);
 
   return (
-    <>
-      <SideNavHeading className={'not-header'}>Billing</SideNavHeading>
-      <SideNavSubButton Icon={<FluentIcon name={'TopSpeed20Regular'} />} to={`/configuration/billing/usage`}>
-        Service usage
-      </SideNavSubButton>
-      <SideNavSubButton
-        Icon={<FluentIcon name={'ReceiptMoney20Regular'} />}
-        to={`/configuration/billing/payments`}
-      >
-        Payments &amp; invoices
-      </SideNavSubButton>
-      <SideNavHeading>Security</SideNavHeading>
-      <SideNavSubButton
-        Icon={<FluentIcon name={'Key20Regular'} />}
-        to={`/configuration/security/tokens-secrets`}
-      >
-        Secrets
-      </SideNavSubButton>
-      <SideNavHeading>Schemas</SideNavHeading>
-      {(!collections || collections?.length === 0) && !navigator.onLine ? (
-        <Offline variant={'small'} />
-      ) : (
-        [...(collections || [])]
-          ?.sort((a, b) => a.name.localeCompare(b.name))
-          .map(({ name }, index) => {
-            return (
-              <SideNavSubButton
-                key={index}
-                Icon={<FluentIcon name={'CircleSmall20Filled'} />}
-                to={`/configuration/schema/${name}#0`}
-              >
-                {name}
-              </SideNavSubButton>
-            );
-          })
-      )}
-    </>
+    <Wrapper>
+      {CreateWindow}
+      <div>
+        <SideNavHeading className={'not-header'}>Billing</SideNavHeading>
+        <SideNavSubButton Icon={<FluentIcon name={'TopSpeed20Regular'} />} to={`/configuration/billing/usage`}>
+          Service usage
+        </SideNavSubButton>
+        <SideNavSubButton
+          Icon={<FluentIcon name={'ReceiptMoney20Regular'} />}
+          to={`/configuration/billing/payments`}
+        >
+          Payments &amp; invoices
+        </SideNavSubButton>
+        <SideNavHeading>Security</SideNavHeading>
+        <SideNavSubButton
+          Icon={<FluentIcon name={'Key20Regular'} />}
+          to={`/configuration/security/tokens-secrets`}
+        >
+          Secrets
+        </SideNavSubButton>
+        <SideNavHeading>Schemas</SideNavHeading>
+        {(!collections || collections?.length === 0) && !navigator.onLine ? (
+          <Offline variant={'small'} />
+        ) : (
+          [...(collections || [])]
+            ?.sort((a, b) => a.name.localeCompare(b.name))
+            .map(({ name }, index) => {
+              return (
+                <SideNavSubButton
+                  key={index}
+                  Icon={<FluentIcon name={'CircleSmall20Filled'} />}
+                  to={`/configuration/schema/${name}#0`}
+                >
+                  {name}
+                </SideNavSubButton>
+              );
+            })
+        )}
+      </div>
+      <Actions>
+        <Button onClick={showCreateWindow}>New schema</Button>
+      </Actions>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-itmes: flex-start;
+  justify-content: flex-start;
+  height: 100%;
+  > div:nth-of-type(1) {
+    flex-grow: 1;
+  }
+  > div:nth-of-type(2) {
+    flex-shrink: 0;
+  }
+`;
+
+const Actions = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 6px;
+  border-top: 1px solid ${({ theme }) => theme.color.neutral[theme.mode][200]};
+  padding: 16px 16px;
+  box-sizing: border-box;
+  justify-content: flex-end;
+`;
 
 export { ConfigurationNavigation };
