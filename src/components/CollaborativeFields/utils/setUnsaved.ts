7d438504f0c8a21d93d548e6e2a289e6d3c79f81
Jack Buehner <git@jackbuehner.com>
2022-08-15 01:10:51 -0400

    fix: do not include combobox entry fields in unsaved fields

--- src/components/CollaborativeFields/utils/setUnsaved.ts
+++ src/components/CollaborativeFields/utils/setUnsaved.ts
@@ -3,7 +3,7 @@ import * as Y from 'yjs';
 function setUnsaved(ydoc: Y.Doc | undefined, fieldName: string) {
   const __unsavedFields = ydoc?.getArray('__unsavedFields');
   const unsavedFields = __unsavedFields?.toArray();
-  if (unsavedFields && !unsavedFields.includes(fieldName)) {
+  if (unsavedFields && !unsavedFields.includes(fieldName) && !unsavedFields.includes('__comboboxEntry')) {
     __unsavedFields?.push([fieldName]);
   }
 }
