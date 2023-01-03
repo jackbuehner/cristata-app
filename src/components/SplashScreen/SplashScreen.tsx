06ae321cbc4ab9d44ac6309390a33071c8e50c06
Jack Buehner <git@jackbuehner.com>
2023-01-03 14:58:03 -0500

    feat: rename clear data to reset app

--- src/components/SplashScreen/SplashScreen.tsx
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -309,7 +309,7 @@ function SplashScreen(props: ISplashScreen) {
                     )}`;
                   }}
                 >
-                  Clear data
+                  Reset app
                 </Button>
               </div>
             </>
