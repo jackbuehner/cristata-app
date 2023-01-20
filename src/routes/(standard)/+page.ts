eb7fb36b9a425d9af9ef190a9659d7b5e758b47b
Jack Buehner <git@jackbuehner.com>
2023-01-19 21:23:25 -0500

    fix: reenable redirect to tenant

--- src/routes/(standard)/+page.ts
+++ src/routes/(standard)/+page.ts
@@ -5,5 +5,5 @@ export const load = (async ({ parent }) => {
   const { authUser } = await parent();
 
   // redirect to tenant path based on the authenticated user
-  // throw redirect(307, `/${authUser.tenant}`);
+  throw redirect(307, `/${authUser.tenant}`);
 }) satisfies PageLoad;
