aaecab137a24eb5a265047a8dadcf6f222344e60
Jack Buehner <git@jackbuehner.com>
2023-01-26 15:23:04 -0500

    feat: hide app icon on macos

--- src/lib/main-layout/Titlebar.svelte
+++ src/lib/main-layout/Titlebar.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   import { server } from '$utils/constants';
-  import { Button, Flyout, IconButton, PersonPicture, TextBlock } from 'fluent-svelte';
+  import { Flyout, IconButton, PersonPicture, TextBlock } from 'fluent-svelte';
   import type { LayoutData } from '../../routes/(standard)/[tenant]/$types';
   import Profile from './Profile.svelte';
 
   export let data: LayoutData;
 
-  //@ts-expect-error windowControlsOverlay is only available in some browsers
-  // if the horizontal offset is greater than 0, this means the window controls are on the left
-  const customTitlebarOffsetX = navigator.windowControlsOverlay?.getBoundingClientRect?.().x || 0;
+  //@ts-expect-error userAgentData is only available in some browsers
+  const isMacLike = navigator.userAgentData
+    ? /(Mac|iPhone|iPod|iPad)/i.test(navigator.userAgentData.platform)
+    : /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
 
   let flyoutOpen: boolean = false;
 
@@ -18,20 +19,22 @@
 
 <div class="titlebar">
   <div class="left">
-    <svg xmlns="http://www.w3.org/2000/svg" width="41.57" height="26" viewBox="0 0 31.1775 36">
-      <path
-        d="m28.1553 10.7445-8.1515-4.7059v12.7647l8.1515-4.7059zM7.4376 8.1969l11.0557 6.3824V5.1667l-2.9039-1.676ZM12.683 30.8327l2.9064 1.677 8.081-4.665-10.9852-6.3409zM25.182 26.9724l2.9736-1.7166v-9.4132l-11.1275 6.424zM5.9264 9.0687l-2.903 1.6758-.0006 9.412 11.0544-6.3825zM3.0229 25.2555l8.1495 4.704.0028-12.764-8.1521 4.706z"
-      />
-      <path
-        d="M15.589 0 .0006 8.9998 0 27.0002 15.5886 36l15.5885-8.9998V8.9998zm14.0775 26.1277L15.5897 34.255l-14.078-8.1273.0005-16.2554L15.5896 1.745l14.0767 8.1273z"
-      />
-    </svg>
+    {#if !isMacLike}
+      <svg xmlns="http://www.w3.org/2000/svg" width="41.57" height="26" viewBox="0 0 31.1775 36">
+        <path
+          d="m28.1553 10.7445-8.1515-4.7059v12.7647l8.1515-4.7059zM7.4376 8.1969l11.0557 6.3824V5.1667l-2.9039-1.676ZM12.683 30.8327l2.9064 1.677 8.081-4.665-10.9852-6.3409zM25.182 26.9724l2.9736-1.7166v-9.4132l-11.1275 6.424zM5.9264 9.0687l-2.903 1.6758-.0006 9.412 11.0544-6.3825zM3.0229 25.2555l8.1495 4.704.0028-12.764-8.1521 4.706z"
+        />
+        <path
+          d="M15.589 0 .0006 8.9998 0 27.0002 15.5886 36l15.5885-8.9998V8.9998zm14.0775 26.1277L15.5897 34.255l-14.078-8.1273.0005-16.2554L15.5896 1.745l14.0767 8.1273z"
+        />
+      </svg>
+    {/if}
     <TextBlock variant="caption">Cristata (Preview) – {data.authUser.tenant}</TextBlock>
   </div>
   <div class="right">
     <div class="account">
       <Flyout placement="bottom" alignment="end" bind:open={flyoutOpen}>
-        <IconButton style="padding: 2px; margin-right: 16px;">
+        <IconButton style="padding: 2px;">
           <PersonPicture
             size={26}
             src="{server.location}/v3/{data.authUser.tenant}/user-photo/{data.authUser._id}"
@@ -61,6 +64,8 @@
     user-select: none;
     justify-content: space-between;
     background-color: #f3f3f3;
+    padding: 0 16px;
+    box-sizing: border-box;
   }
 
   .left {
@@ -80,7 +85,7 @@
   svg {
     width: 16px;
     height: 16px;
-    margin: 0 16px;
+    margin: 0 16px 0 0;
     fill: var(--color-primary-800);
   }
   @media (prefers-color-scheme: dark) {
