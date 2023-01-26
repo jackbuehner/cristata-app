0b0fa59c71b9df5f44953f67bca9a1aec1fd330b
Jack Buehner <git@jackbuehner.com>
2023-01-25 22:34:33 -0500

    fix: pass setThemeMode to playground page

--- src/routes/(standard)/[tenant]/playground/+page.svelte
+++ src/routes/(standard)/[tenant]/playground/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
   import { Playground as PlaygroundPage } from '$react/playground';
+  import { themeMode } from '$stores/themeMode';
+
+  function setThemeMode(mode: unknown) {
+    if (mode === 'light' || mode === 'dark') {
+      $themeMode = mode;
+    }
+  }
 </script>
 
-<react:PlaygroundPage />
+<react:PlaygroundPage {setThemeMode} />
