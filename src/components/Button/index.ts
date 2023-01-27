3d12696a44c5ceb7fe37bcb88b4d0164db181889
Jack Buehner <git@jackbuehner.com>
2023-01-26 22:44:50 -0500

    feat: remove old nav button components

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,7 +1,5 @@
 import { Button } from './Button';
 import { buttonEffect } from './buttonEffect';
 import { IconButton } from './IconButton';
-import { SideNavMainButton } from './SideNavMainButton';
-import { SideNavSubButton } from './SideNavSubButton';
 
-export { Button, buttonEffect, IconButton, SideNavMainButton, SideNavSubButton };
+export { Button, buttonEffect, IconButton };
