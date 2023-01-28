e34835bb582331413c61e7c00046b35a096c18e6
Jack Buehner <git@jackbuehner.com>
2023-01-28 14:36:28 -0500

    feat: support caching with variables and returning store instead of direct data

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,8 +1,9 @@
 import { browser } from '$app/environment';
 import { server } from '$utils/constants';
-import { hasKey } from '$utils/hasKey';
-import { getOperationAST, isSelectionNode, print, type DocumentNode } from 'graphql';
-import { get, writable } from 'svelte/store';
+import { flattenObject } from 'flatten-anything';
+import { getOperationAST, print, type DocumentNode } from 'graphql';
+import type { Readable } from 'svelte/store';
+import { derived, get, readable, writable } from 'svelte/store';
 
 export interface GraphqlQueryOptions<VariablesType> {
   tenant: string;
@@ -14,18 +15,27 @@ export interface GraphqlQueryOptions<VariablesType> {
    * If it is not invalid, a persisted copy from localStorage will be used.
    */
   persistCache?: number;
+  /**
+   * Millseconds until the cache is considered outdated.
+   */
+  expireCache?: number;
   fetchNextPages?: boolean;
   skip?: boolean;
 }
 
 // cache store
 const localStorePersistCopy = (browser && localStorage.getItem('store:graphql:cache')) || '{}';
-const cache = writable<Record<string, { time: number; value: Record<string, unknown>; persist: boolean }>>(
-  JSON.parse(localStorePersistCopy)
-);
+const cache = writable<
+  Record<string, Record<string, { time: number; value: ReturnType<unknown>; persist: boolean }>>
+>(JSON.parse(localStorePersistCopy));
 cache.subscribe((value) => {
+  // persist to localStorage only if persist is true
   if (browser) {
-    const persistable = Object.fromEntries(Object.entries(value).filter(([key, value]) => value.persist));
+    const persistable = Object.fromEntries(
+      Object.entries(value).map(([k, v]) => {
+        return [k, Object.fromEntries(Object.entries(v).filter(([key, value]) => value.persist))];
+      })
+    );
     localStorage.setItem('store:graphql:cache', JSON.stringify(persistable));
   }
 });
@@ -33,7 +43,7 @@ cache.subscribe((value) => {
 export async function query<DataType = unknown, VariablesType = unknown>(
   opts: GraphqlQueryOptions<VariablesType>
 ): Promise<GraphqlQueryReturn<DataType>> {
-  if (opts?.skip) return {};
+  if (opts.skip) return {};
 
   const operation = getOperationAST(opts.query);
   const operationName = operation && operation.name && operation.name.value;
@@ -42,16 +52,17 @@ export async function query<DataType = unknown, VariablesType = unknown>(
     // @ts-expect-error these exsit
     return firstSelection?.alias || firstSelection?.name.value || undefined;
   })();
+  const varKey = createVariableKey(opts.variables || {}, opts.tenant);
 
-  if (operationName && get(cache)[operationName]) {
-    if (opts?.persistCache) {
-      const { time, value } = get(cache)[operationName];
-      if (new Date(time + opts.persistCache) > new Date()) {
+  if (operationName && opts.useCache && get(cache)[operationName]?.[varKey]) {
+    if (opts.persistCache || opts.expireCache) {
+      const { time, value } = get(cache)[operationName][varKey];
+      if (new Date(time + (opts.persistCache || opts.expireCache || 0)) > new Date()) {
         // cache data is not yet expired
         return value as ReturnType<DataType>;
       }
     } else {
-      return get(cache)[operationName].value as ReturnType<DataType>;
+      return get(cache)[operationName][varKey].value as ReturnType<DataType>;
     }
   }
 
@@ -82,13 +93,17 @@ export async function query<DataType = unknown, VariablesType = unknown>(
       }
 
       // cache the result so it can be used immediately (cache is lost on page refresh)
-      if (opts?.useCache && operationName)
+      if (operationName)
         cache.update((state) => ({
           ...state,
           [operationName]: {
-            time: new Date().getTime(),
-            value: json as Record<string, unknown>,
-            persist: !!opts.persistCache,
+            ...state[operationName],
+            [varKey]: {
+              time: new Date().getTime(),
+              value: json as Record<string, unknown>,
+              use: opts.useCache,
+              persist: !!opts.persistCache,
+            },
           },
         }));
 
@@ -97,6 +112,54 @@ export async function query<DataType = unknown, VariablesType = unknown>(
 
     return null;
   });
+}
+
+export function getQueryStore<DataType = unknown, VariablesType = unknown>(opts: {
+  queryName: string;
+  variables?: VariablesType;
+  tenant: string;
+}): Readable<ReturnType<DataType>> {
+  const varKey = createVariableKey(opts.variables || {}, opts.tenant);
+  return derived(cache, ($cache) => {
+    if (!$cache[opts.queryName]?.[varKey]) return {};
+    return {
+      data: $cache[opts.queryName]?.[varKey].value.data,
+      errors: $cache[opts.queryName]?.[varKey].value.errors,
+    } as ReturnType<DataType>;
+  });
+}
+
+export async function queryWithStore<DataType = unknown, VariablesType = unknown>(
+  opts: GraphqlQueryOptions<VariablesType> & { waitForQuery?: boolean }
+): Promise<Readable<ReturnType<DataType>>> {
+  await new Promise<void>(async (resolve) => {
+    if (opts.waitForQuery) await query(opts);
+    else query(opts);
+    resolve();
+  });
+
+  const operation = getOperationAST(opts.query);
+  const operationName = (operation && operation.name && operation.name.value) || undefined;
+  if (!operationName) return readable({});
+
+  return getQueryStore<DataType, VariablesType>({
+    queryName: operationName,
+    variables: opts.variables,
+    tenant: opts.tenant,
+  });
+}
+
+/**
+ * Flattens, alphabetically sorts, and stringifies the variables object.
+ *
+ * This is useful for creating predictable indentifiers for different sets
+ * of variables.
+ */
+function createVariableKey(variables: Record<string, unknown>, tenant?: string) {
+  const flat = flattenObject(variables);
+  if (tenant) flat.tenant = tenant; // ensure tenant is included as a variable so different tenants do not overwrite their cache
+  const sorted = Object.fromEntries(Object.entries(flat).sort((a, b) => a[0].localeCompare(b[0])));
+  return JSON.stringify(sorted);
 }
 
 export { cache as queryCacheStore };
