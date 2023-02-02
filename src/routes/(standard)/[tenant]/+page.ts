1a20e4190030a4f02448d04425b35cdc951885e3
Jack Buehner <git@jackbuehner.com>
2023-02-01 23:49:27 -0500

    fix: use param instead of authUser

--- src/routes/(standard)/[tenant]/+page.ts
+++ src/routes/(standard)/[tenant]/+page.ts
@@ -9,27 +9,25 @@ import {
 import { query } from '$graphql/query';
 import type { PageLoad } from './$types';
 
-export const load = (async ({ parent, fetch }) => {
-  const { authUser } = await parent();
-
+export const load = (async ({ params, fetch }) => {
   // get the configuration
-  const dashboardConfig = await query<DashboardConfigQuery, DashboardConfigQueryVariables>({
+  const dashboardConfig = query<DashboardConfigQuery, DashboardConfigQueryVariables>({
     fetch,
-    tenant: authUser.tenant,
+    tenant: params.tenant,
     query: DashboardConfig,
     useCache: true,
   });
 
   // get the number of documents in each stage
-  const workflowCounts = await query<WorkflowCountsQuery, WorkflowCountsQueryVariables>({
+  const workflowCounts = query<WorkflowCountsQuery, WorkflowCountsQueryVariables>({
     fetch,
-    tenant: authUser.tenant,
+    tenant: params.tenant,
     query: WorkflowCounts,
     useCache: true,
   });
 
   return {
-    dashboardConfig: dashboardConfig?.data?.configuration?.dashboard,
-    workflowCounts: workflowCounts?.data?.workflow || undefined,
+    dashboardConfig: (await dashboardConfig)?.data?.configuration?.dashboard,
+    workflowCounts: (await workflowCounts)?.data?.workflow || undefined,
   };
 }) satisfies PageLoad;
