d76aca08a393f92fd3eff6d1f8541a80686c8574
Jack Buehner <git@jackbuehner.com>
2023-02-01 23:25:46 -0500

    fix: hide splash screen when error page is shown

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,65 +4,68 @@
   import { persistor } from '../redux/store';
 </script>
 
-{#if $page.status === 401}
-  <div class="errorwrapper">
-    <div class="errorblock">
-      <p>{$page.error?.message}</p>
-      <p style="font-size: 14px; font-style: italic;">{`\n Build date: ${now.toISOString()}`}</p>
-      <button on:click={() => (window.location.href = window.location.href)}>Try again</button>
+<div class="errorwrapper">
+  <div class="errorblock">
+    <p>{$page.error?.message}</p>
+    <p style="font-size: 14px; font-style: italic;">{`\n Build date: ${now.toISOString()}`}</p>
+    <button on:click={() => (window.location.href = window.location.href)}>Try again</button>
+    <button
+      on:click={async () => {
+        await persistor.purge(); // clear persisted redux store
+        // await client.clearStore(); // clear persisted apollo data
+        window.localStorage.removeItem('apollo-cache-persist'); // apollo doesn't always clear this
+        const tenant = window.location.pathname.split('/')[1];
+        window.location.href = `${import.meta.env.VITE_API_PROTOCOL}//${import.meta.env.VITE_AUTH_BASE_URL}/${
+          tenant || ''
+        }/sign-out?return=${encodeURIComponent(window.location.origin + '/' + tenant)}`;
+      }}>Sign out</button
+    >
+    <div>
       <button
         on:click={async () => {
           await persistor.purge(); // clear persisted redux store
           // await client.clearStore(); // clear persisted apollo data
           window.localStorage.removeItem('apollo-cache-persist'); // apollo doesn't always clear this
+          if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.getRegistrations().then((registrations) => {
+              for (let registration of registrations) {
+                registration.unregister();
+              }
+            });
+          }
+          window.location.href = window.location.href;
+        }}>Clear cache</button
+      >
+      <button
+        on:click={async () => {
+          await persistor.purge(); // clear persisted redux store
+          // await client.clearStore(); // clear persisted apollo data
+          window.localStorage.clear();
+          window.sessionStorage.clear();
+          if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.getRegistrations().then((registrations) => {
+              for (let registration of registrations) {
+                registration.unregister();
+              }
+            });
+          }
           const tenant = window.location.pathname.split('/')[1];
           window.location.href = `${import.meta.env.VITE_API_PROTOCOL}//${import.meta.env.VITE_AUTH_BASE_URL}/${
             tenant || ''
           }/sign-out?return=${encodeURIComponent(window.location.origin + '/' + tenant)}`;
-        }}>Sign out</button
+        }}>Reset app</button
       >
-      <div>
-        <button
-          on:click={async () => {
-            await persistor.purge(); // clear persisted redux store
-            // await client.clearStore(); // clear persisted apollo data
-            window.localStorage.removeItem('apollo-cache-persist'); // apollo doesn't always clear this
-            if ('serviceWorker' in navigator) {
-              navigator.serviceWorker.getRegistrations().then((registrations) => {
-                for (let registration of registrations) {
-                  registration.unregister();
-                }
-              });
-            }
-            window.location.href = window.location.href;
-          }}>Clear cache</button
-        >
-        <button
-          on:click={async () => {
-            await persistor.purge(); // clear persisted redux store
-            // await client.clearStore(); // clear persisted apollo data
-            window.localStorage.clear();
-            window.sessionStorage.clear();
-            if ('serviceWorker' in navigator) {
-              navigator.serviceWorker.getRegistrations().then((registrations) => {
-                for (let registration of registrations) {
-                  registration.unregister();
-                }
-              });
-            }
-            const tenant = window.location.pathname.split('/')[1];
-            window.location.href = `${import.meta.env.VITE_API_PROTOCOL}//${
-              import.meta.env.VITE_AUTH_BASE_URL
-            }/${tenant || ''}/sign-out?return=${encodeURIComponent(window.location.origin + '/' + tenant)}`;
-          }}>Reset app</button
-        >
-      </div>
     </div>
   </div>
-{:else}
-  <h1>{$page.status}</h1>
-  <p>{$page.error?.message}</p>
-{/if}
+</div>
+
+<div>
+  <style>
+    .root-splash-wrapper {
+      display: none !important;
+    }
+  </style>
+</div>
 
 <style>
   div.errorwrapper {
