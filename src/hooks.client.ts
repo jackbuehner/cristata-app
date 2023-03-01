6431d765f8716ee22524d73e822cbe07199e8f7f
Jack Buehner <git@jackbuehner.com>
2023-03-01 14:27:22 -0500

    feat: add sentry replays support

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,20 +1,21 @@
 import { VITE_BUILD_START_DATE_TIME, VITE_SENTRY_DSN } from '$env/static/public';
-import { BasicProfileMe, type BasicProfileMeQuery } from '$graphql/graphql';
-import { getQueryStore } from '$graphql/query';
-import { authCache } from '$stores/authCache';
 import { getUserForSentry } from '$utils/getUserForSentry';
-import type { User as SentryUser } from '@sentry/svelte';
 import * as SentrySvelte from '@sentry/svelte';
 import { BrowserTracing } from '@sentry/tracing';
 import type { HandleClientError } from '@sveltejs/kit';
-import { getOperationAST } from 'graphql';
-import { get as getStoreValue } from 'svelte/store';
 
 SentrySvelte.init({
   dsn: VITE_SENTRY_DSN,
-  integrations: [new BrowserTracing()],
+  integrations: [new BrowserTracing(), new SentrySvelte.Replay()],
   tracesSampleRate: 1.0,
   environment: import.meta.env.MODE,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
 });
 
 SentrySvelte.setTag('svelteKit', 'browser');
