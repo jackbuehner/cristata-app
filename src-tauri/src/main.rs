32b3f2dfd95a939cb32da99b300e001e60528c34
Jack Buehner <git@jackbuehner.com>
2023-03-06 18:51:42 -0500

    feat: support windows border and accent colors

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,9 +15,15 @@ fn main() {
         .setup(|app| {
             let main_window = app.get_window("main").unwrap();
 
+            // apply window shadows, which also ensures that corners are correctly rounded
+            #[cfg(any(windows, target_os = "macos"))]
+            window_shadows::set_shadow(&main_window, true).unwrap();
+
             #[cfg(target_os = "windows")]
             // read system info from the registry
             let hklm = RegKey::predef(winreg::enums::HKEY_LOCAL_MACHINE);
+
+            // read the build number and version number
             let cur_ver = hklm.open_subkey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion")?;
             let reg_build_number: String = cur_ver.get_value("CurrentBuildNumber")?;
             let build_number: u32 = reg_build_number.parse().unwrap();
@@ -38,7 +44,7 @@ fn main() {
 
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, set_window_color, get_accent_color])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 
@@ -48,4 +54,139 @@ fn main() {
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
+}
+
+// Gets the windows accent color.
+fn get_window_accent_color() -> u32 {
+    let hkcu = RegKey::predef(winreg::enums::HKEY_CURRENT_USER);
+    let dwm = hkcu.open_subkey("SOFTWARE\\Microsoft\\Windows\\DWM")
+        .expect("error while getting DWM registry");
+
+    let color_accent: u32 = dwm.get_value("AccentColor").unwrap_or(0xffb93854);
+    return color_accent;
+}
+
+/// Gets the correct accent color for the window border based on whether it
+/// is active or inactive. Specify `transparent` as `false` to get
+/// a solid color that can be used for borders.
+fn get_window_border_color(active: bool, transparent: bool, theme: tauri::Theme) -> u32 {
+    let hkcu = RegKey::predef(winreg::enums::HKEY_CURRENT_USER);
+    let dwm = hkcu.open_subkey("SOFTWARE\\Microsoft\\Windows\\DWM")
+        .expect("error while getting DWM registry");
+
+    // read whether window border colors are enabled
+    let color_prevalence: u32 = dwm.get_value("ColorPrevalence").unwrap_or(0);
+
+    if color_prevalence == 1 && active == true {
+        let color_accent = get_window_accent_color();
+        if transparent { 
+            return color_accent;
+        } else { 
+            return color_accent - 4278190080;
+        }
+    } else {
+        if transparent {
+            if theme == tauri::Theme::Dark {
+                return 0xff383838;
+            } else {
+                return 0xffb5b5b5;
+            }
+        } else {
+            if theme == tauri::Theme::Dark {
+                return 0x00383838;
+            } else {
+                return 0x00b5b5b5;
+            }
+        }
+    }
+}
+
+/// Gets the theme mode for a window: Light or Dark.
+///
+/// If a theme mode cannot be found, it defaults to light.
+fn get_theme(window: &tauri::Window) -> tauri::Theme {
+    let theme = window.theme().unwrap_or_else(|_| { tauri::Theme::Light });
+    return theme
+}
+
+#[tauri::command]
+fn set_window_color(window: tauri::Window, active: bool) {
+    use raw_window_handle::HasRawWindowHandle;
+    
+    match window.raw_window_handle() {
+        #[cfg(target_os = "windows")]
+        raw_window_handle::RawWindowHandle::Win32(handle) => {
+            let theme = get_theme(&window);
+            let border_color = get_window_border_color(active, false, theme);
+
+            use windows::Win32::{
+                Graphics::Dwm::{
+                    DwmSetWindowAttribute,
+                    DWMWA_BORDER_COLOR,
+                },
+                Foundation::HWND
+            };
+        
+            unsafe {
+                // convice rust that handle.hwnd is actually of type HWND
+                let hwnd: HWND = std::mem::transmute(handle.hwnd);
+
+                // set the border color
+                DwmSetWindowAttribute(
+                    hwnd,
+                    DWMWA_BORDER_COLOR,
+                    &border_color as *const _ as _,
+                    std::mem::size_of::<u32>().try_into().unwrap()
+                ).unwrap_or_else(|err| println!("{:?}", err));
+            }
+        }
+        _ => {},
+    }
+
+}
+
+#[tauri::command]
+fn get_accent_color(window: tauri::Window) -> String {
+    use raw_window_handle::HasRawWindowHandle;
+
+    let theme = get_theme(&window);
+    let default_light_mode_accent = "rgb(84, 56, 185)";
+    let default_dark_mode_accent = "rgb(181, 173, 235)";
+    
+    match window.raw_window_handle() {
+        #[cfg(target_os = "windows")]
+        raw_window_handle::RawWindowHandle::Win32(_) => {
+            use windows::UI::ViewManagement::{ UISettings, UIColorType };
+            let ui_settings = UISettings::new()
+                .expect("error getting ui settings");
+
+            if theme == tauri::Theme::Dark {
+                match ui_settings.GetColorValue(UIColorType::AccentLight2) {
+                    Ok(color) => {
+                        return format!("rgb({}, {}, {})", color.R, color.G, color.B);
+                    }
+                    _ => {
+                        return default_dark_mode_accent.to_owned();
+                    }
+                }
+            } else {
+                match ui_settings.GetColorValue(UIColorType::Accent) {
+                    Ok(color) => {
+                        return format!("rgb({}, {}, {})", color.R, color.G, color.B);
+                    }
+                    _ => {
+                        return default_light_mode_accent.to_owned();
+                    }
+                }
+            }
+        }
+        _ => {
+            if theme == tauri::Theme::Dark {
+                return default_dark_mode_accent.to_owned();
+            } else {
+                return default_light_mode_accent.to_owned();
+            }
+        },
+    }
+
 }
