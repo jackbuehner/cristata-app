b0b2a35eba2d6decec0356353deebafb0ba58198
Jack Buehner <git@jackbuehner.com>
2023-03-15 00:36:32 -0400

    feat: add FieldWrapper component

---
+++ src/lib/common/Field/Wrapper.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { TextBlock } from 'fluent-svelte';
+
+  export let label: string;
+  export let forId: string;
+</script>
+
+<div class="field" data-for={forId} class:nocaption={!$$slots.caption}>
+  <div class="field-label">
+    <TextBlock tag="span">{label}</TextBlock>
+  </div>
+  {#if $$slots.caption}
+    <label for={forId}>
+      <TextBlock variant="caption" tag="span">
+        <slot name="caption" />
+      </TextBlock>
+    </label>
+  {/if}
+  <slot />
+</div>
+
+<style>
+  div.field {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+  div.field:last-of-type {
+    margin-bottom: 10px;
+  }
+  div.field.nocaption > .field-label {
+    margin-bottom: 6px;
+  }
+  div.field > label {
+    margin-bottom: 6px;
+    opacity: 0.8;
+  }
+</style>
---
+++ src/lib/common/Field/index.ts
@@ -0,0 +1,1 @@
+export { default as FieldWrapper } from './Wrapper.svelte';
