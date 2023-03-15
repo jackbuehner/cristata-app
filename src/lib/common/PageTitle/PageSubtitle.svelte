08523a7324a1701bcadedd2b57b0a1e532cfa6f3
Jack Buehner <git@jackbuehner.com>
2023-03-15 00:36:44 -0400

    feat: support caption for page subtitle component

--- src/lib/common/PageTitle/PageSubtitle.svelte
+++ src/lib/common/PageTitle/PageSubtitle.svelte
@@ -2,16 +2,29 @@
   import { TextBlock } from 'fluent-svelte';
 
   export let style: string = '';
+  export let caption: string = '';
 </script>
 
 <h2>
   <TextBlock variant="subtitle" tag="span" {style}><slot /></TextBlock>
 </h2>
 
+{#if caption}
+  <p>
+    <TextBlock tag="span">{caption}</TextBlock>
+  </p>
+{/if}
+
 <style>
-  h2 {
-    margin: 25px auto 10px auto;
+  h2,
+  p {
     padding: 0 20px;
     max-width: 1000px;
+  }
+  h2 {
+    margin: 25px auto 10px auto;
+  }
+  p {
+    margin: -8px auto 10px auto;
   }
 </style>
