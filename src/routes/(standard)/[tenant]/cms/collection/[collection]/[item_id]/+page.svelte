a04cd1933e588a1c5fea52f0c05dfce4ed9de444
Jack Buehner <git@jackbuehner.com>
2023-03-25 16:56:44 -0400

    feat: use photo field descriptions on colllection item page

--- src/routes/(standard)/[tenant]/cms/collection/[collection]/[item_id]/+page.svelte
+++ src/routes/(standard)/[tenant]/cms/collection/[collection]/[item_id]/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import { CollectionItemPage } from '$react/CMS/CollectionItemPage';
+  import type { PageData } from './$types';
+
+  export let data: PageData;
 </script>
 
-<react:CollectionItemPage />
+<react:CollectionItemPage {data} />
