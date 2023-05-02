a46c2a56c4673701d9397f6f4f99bce0ac7b475b
Jack Buehner <git@jackbuehner.com>
2023-05-02 19:48:14 -0400

    feat: support field wrapper description prop

--- src/lib/common/Field/Wrapper.svelte
+++ src/lib/common/Field/Wrapper.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
+  import dompurify from 'dompurify';
   import { TextBlock } from 'fluent-svelte';
 
   export let label: string;
   export let forId: string;
+
+  /**
+   * A HTML string description for the field. Optionally, a more complex
+   * description can be provided via the caption slot
+   */
+  export let description: string = '';
+
+  $: hasCaption = !!$$slots.caption || !!description;
 </script>
 
-<div class="field" data-for={forId} class:nocaption={!$$slots.caption}>
+<div class="field" data-for={forId} class:nocaption={!hasCaption}>
   <div class="field-label">
-    <TextBlock tag="span">{label}</TextBlock>
+    <TextBlock tag="span">{@html dompurify.sanitize(label)}</TextBlock>
   </div>
-  {#if $$slots.caption}
+  {#if hasCaption}
     <label for={forId}>
       <TextBlock variant="caption" tag="span">
-        <slot name="caption" />
+        {#if $$slots.caption}
+          <slot name="caption" />
+        {:else}
+          {@html dompurify.sanitize(description)}
+        {/if}
       </TextBlock>
     </label>
   {/if}
