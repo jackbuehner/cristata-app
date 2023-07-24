1e10a00ca416e8c794b9ee172ce02e50225eedba
Jack Buehner <git@jackbuehner.com>
2023-07-23 22:27:39 -0400

    refactor: move processSchemeDefs to own util

--- src/routes/(standard)/[tenant]/cms/collection/[collection]/[item_id]/+layout.ts
+++ src/routes/(standard)/[tenant]/cms/collection/[collection]/[item_id]/+layout.ts
@@ -4,6 +4,7 @@ import { server } from '$utils/constants';
 import { deepen } from '$utils/deepen';
 import { genAvatar } from '$utils/genAvatar';
 import { getProperty } from '$utils/objectPath';
+import { processSchemaDef as _processSchemaDef } from '$utils/processSchemaDef';
 import { isTypeTuple, type DeconstructedSchemaDefType } from '@jackbuehner/cristata-generator-schema';
 import { notEmpty, slugify, uncapitalize } from '@jackbuehner/cristata-utils';
 import _ColorHash from 'color-hash';
@@ -82,54 +83,7 @@ export const load = (async ({ parent, params, url }) => {
     showHidden?: boolean;
   }): DeconstructedSchemaDefType => {
     const { schemaDef, ...opts } = args || {};
-
-    return (
-      (schemaDef || collection.deconstructedSchema)
-        .map(([key, def]) => {
-          const labelDef = def.docs?.find(([ckey]) => ckey.replace(key + '.', '') === '#label')?.[1];
-          return [key, def, labelDef] as [string, typeof def, typeof labelDef];
-        })
-        // sort fields to match their order
-        .sort((a, b) => {
-          const orderA = parseInt(`${a[1].field?.order || 1000}`);
-          const orderB = parseInt(`${b[1].field?.order || 1000}`);
-          return orderA > orderB ? 1 : -1;
-        })
-        // hide hidden fields
-        .filter(([, def, labelDef]) => {
-          if (opts.showHidden) return true;
-          if (opts.isPublishModal) return def.field?.hidden === 'publish-only';
-          if (labelDef) return labelDef.field?.hidden !== true && def.field?.hidden !== 'publish-only';
-          return def.field?.hidden !== true && def.field?.hidden !== 'publish-only';
-        })
-        // remove fields that are used in the sidebar
-        .filter(([key]) => {
-          if (key === 'stage') return false;
-          if (key === 'permissions.users') return false;
-          if (key === 'permissions.teams') return false;
-          return true;
-        })
-        // remove timestamps related to publishing
-        .filter(([key]) => {
-          return key !== 'timestamps.published_at' && key !== 'timestamps.updated_at';
-        })
-        .filter(([key, def, labelDef]) => {
-          if (opts.collapsed === true)
-            return def.field?.collapsed === true || labelDef?.field?.collapsed === true;
-          if (opts.collapsed === false)
-            return def.field?.collapsed !== true && labelDef?.field?.collapsed !== true;
-          return true;
-        })
-        .map(([key, def, labelDef]) => {
-          if (labelDef && labelDef.field) {
-            return [key, { ...def, field: { ...(def.field || {}), ...labelDef.field } }] as [
-              string,
-              typeof def
-            ];
-          }
-          return [key, def] as [string, typeof def];
-        })
-    );
+    return _processSchemaDef({ schemaDef: schemaDef || collection.deconstructedSchema, ...opts });
   }) satisfies ProcessSchemaDef;
 
   const calcPublishPermissions = (params: CalcPublishPermissionsParams): CalcPublishPermissionsReturn => {
